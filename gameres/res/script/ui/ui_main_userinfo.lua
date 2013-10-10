e838f15508bea42cfca9de0d5042e39168d895fa
sgkqk <kqk@qq.com>
2013-10-10 16:55:01 +0800

    主界面xui文件与脚本文件提交

---
+++ gameres/res/script/ui/ui_main_userinfo.lua
@@ -0,0 +1,12 @@
+--------------------------------------------------------------
+-- FileName:  main_userinfo.lua
+-- author: 此文件為自動生成，沒有作者。有問題請 @郭浩
+--------------------------------------------------------------
+ui_main_userinfo = {};
+local p =  ui_main_userinfo;
+p.ID_MAIN_TEXT_ITEMNUM				    = 7;	--玩家持有物品数量及上限
+p.ID_MAIN_TEXT_CARDNUM				    = 5;	--玩家持有卡牌数量及上限
+p.ID_MAIN_TEXT_NAME2					     = 4;	--玩家称号
+p.ID_MAIN_TEXT_MONEY					     = 18;	--金币数量
+p.ID_MAIN_TEXT_COIN					      = 17;	--代币数量
+p.ID_MAIN_PICTURE_UERINFO_M			= 16;
