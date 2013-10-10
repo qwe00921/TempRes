56259d28bfc4d05fafb5b1b686d6c9b93aa1b91a
sgkqk <kqk@qq.com>
2013-10-10 12:06:56 +0800

    提交界面美术替代资源

---
+++ gameres/res/script/ui/ui_login_main.lua
@@ -0,0 +1,14 @@
+--------------------------------------------------------------
+-- FileName:  login_main.lua
+-- author: 此文件為自動生成，沒有作者。有問題請 @郭浩
+--------------------------------------------------------------
+ui_login_main = {};
+local p =  ui_login_main;
+p.ID_LOGIN_BUTTON_STARTGAME			= 15;	--开始游戏按钮
+p.ID_LOGIN_TEXT_SEVERSELECT			= 14;	--玩家服务器选择信息
+p.ID_LOGIN_BUTTON_SEVERSELECT			= 13;	--玩家选择服务器按钮
+p.ID_LOGIN_TEXT_IDINPUT				   = 11;	--玩家账号选择信息
+p.ID_LOGIN_TXTSEVER					      = 9;	--提示文字（选择服务器）
+p.ID_LOGIN__BUTTON_ID					    = 7;	--玩家选择账号按钮
+p.ID_LOGIN_TEXT_ID					       = 2;	--提示文字（输入账号）
+p.ID_LOGIN_MAIN_BG					       = 1;
---
+++ gameres/res/script/ui/ui_login_severselect_option.lua
@@ -0,0 +1,9 @@
+--------------------------------------------------------------
+-- FileName:  login_severselect_option.lua
+-- author: 此文件為自動生成，沒有作者。有問題請 @郭浩
+--------------------------------------------------------------
+ui_login_severselect_option = {};
+local p =  ui_login_severselect_option;
+p.ID_LOGIN_CTRL_TEXT_SEVEROPTION		= 24;	--服务器名称
+p.ID_LOGIN_CTRL_PICTURE_SEVEROPTION	= 22;	--服务器选项底背景
+p.ID_LOGIN_CTRL_BUTTON_SEVEROPTION	= 2;	--服务器按钮
