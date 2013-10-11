906d140aa71525773f3b9f69085b971d95f11b28
sgkqk <kqk@qq.com>
2013-10-11 15:08:01 +0800

    main_interface.lua main_userinfo.lua 文件更新

--- gameres/res/script/ui/ui_main_interface.lua
+++ gameres/res/script/ui/ui_main_interface.lua
@@ -4,14 +4,14 @@
 --------------------------------------------------------------
 ui_main_interface = {};
 local p =  ui_main_interface;
-p.ID_MAIN_LIST_ACT					       = 12;	--活动&广告框
+p.ID_CTRL_MAIN_LIST_ACT				   = 12;	--活动&广告框
 p.ID_CTRL_TEXT_11						       = 11;
-p.ID_MAIN_EXP_ACT						       = 9;	--玩家活力
-p.ID_MAIN_EXP_EXP						       = 8;	--玩家经验值
-p.ID_MAIN_TEXT_LEVEL					     = 7;	--玩家等级
-p.ID_MAIN_CTRL_PICTURE_SHOP			= 26;	--商城按钮
-p.ID_MAIN_CTRL_BUTTON_MEMU			 = 28;	--功能弹出按钮
-p.ID_MAIN_CTRL_CUSTOMIZE_LEVEL		= 27;
-p.ID_MAIN_CTRL_BUTTON_USERINFO		= 25;
-p.ID_MAIN_CTRL_BUTTON_GIFT			 = 24;	--礼包图片
-p.ID_MAIN_CTRL_PICTURE_BG				 = 1;	--背景
+p.ID_CTRL_MAIN_EXP_ACT				    = 9;	--玩家活力
+p.ID_CTRL_MAIN_EXP_EXP				    = 8;	--玩家经验值
+p.ID_CTRL_MAIN_TEXT_LEVEL				 = 7;	--玩家等级
+p.ID_CTRL_MAIN_PICTURE_SHOP			= 26;	--商城按钮
+p.ID_CTRL_MAIN_BUTTON_MEMU			 = 28;	--功能弹出按钮
+p.ID_CTRL_MAIN_CUSTOMIZE_LEVEL		= 27;
+p.ID_CTRL_MAIN_BUTTON_USERINFO		= 25;
+p.ID_CTRL_MAIN_BUTTON_GIFT			 = 24;	--礼包图片
+p.ID_CTRL_MAIN_PICTURE_BG				 = 1;	--背景
