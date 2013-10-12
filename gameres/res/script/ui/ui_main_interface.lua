f717859a09f731f3de27a152371fc58123bc7361
sgkqk <kqk@qq.com>
2013-10-12 15:01:18 +0800

    main_interface.xui    命名补充 main_act&ad.xui   广告与活动xui文件

--- gameres/res/script/ui/ui_main_interface.lua
+++ gameres/res/script/ui/ui_main_interface.lua
@@ -4,10 +4,10 @@
 --------------------------------------------------------------
 ui_main_interface = {};
 local p =  ui_main_interface;
-p.ID_CTRL_TEMP_BUTTON_MAP				 = 13;
-p.ID_CTRL_MAIN_LIST_ACT				   = 12;
-p.ID_CTRL_MAIN_PICTURE_SHOP			= 26;
-p.ID_CTRL_MAIN_BUTTON_MEMU			 = 28;
+p.ID_CTRL_MAIN_BUTTON_SHOP			 = 8;	--商城按钮
+p.ID_CTRL_TEMP_BUTTON_MAP				 = 13;	--关卡地图进入按钮
+p.ID_CTRL_MAIN_LIST_ACT				   = 12;	--活动列表
+p.ID_CTRL_MAIN_BUTTON_MEMU			 = 28;	--菜单弹出按钮
 p.ID_CTRL_MAIN_CUSTOMIZE_LEVEL		= 27;
-p.ID_CTRL_MAIN_BUTTON_GIFT			 = 24;
-p.ID_CTRL_MAIN_PICTURE_BG				 = 1;
+p.ID_CTRL_MAIN_BUTTON_GIFT			 = 24;	--礼物按钮
+p.ID_CTRL_MAIN_PICTURE_BG				 = 1;	--背景
