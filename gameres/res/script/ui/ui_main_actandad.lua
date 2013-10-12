ac58f50d5fb26e5b2e9425de13bad63b444c802b
sgkqk <kqk@qq.com>
2013-10-12 15:47:06 +0800

    ui_main_actandad.lua 文件名修改

--- gameres/res/script/ui/ui_main_actandad.lua
+++ gameres/res/script/ui/ui_main_actandad.lua
@@ -1,7 +1,7 @@
 --------------------------------------------------------------
--- FileName:  main_act&ad.lua
+-- FileName:  main_actANDad.lua
 -- author: 此文件為自動生成，沒有作者。有問題請 @郭浩
 --------------------------------------------------------------
-ui_main_act&ad = {};
-local p =  ui_main_act&ad;
+ui_main_actandad = {};
+local p =  ui_main_actandad;
 p.ID_CTRL_CTRL_BUTTON_24				  = 24;	--活动&广告框
