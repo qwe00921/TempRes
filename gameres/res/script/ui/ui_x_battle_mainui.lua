52eade4bbe4aabcf90a32ca54590bb64ac0cc7e0
GuoHao <hubris6965@hotmail.com>
2013-10-13 21:11:42 +0800

    替換資源

--- gameres/res/script/ui/ui_x_battle_mainui.lua
+++ gameres/res/script/ui/ui_x_battle_mainui.lua
@@ -4,14 +4,6 @@
 --------------------------------------------------------------
 ui_x_battle_mainui = {};
 local p =  ui_x_battle_mainui;
-p.ID_CTRL_TEXT_16						       = 19;
-p.ID_CTRL_TEXT_15						       = 18;
-p.ID_CTRL_PICTURE_15					     = 17;
-p.ID_CTRL_PICTURE_14					     = 16;
-p.ID_CTRL_PICTURE_12					     = 14;
-p.ID_CTRL_PICTURE_11					     = 13;
-p.ID_CTRL_PICTURE_7					      = 7;
-p.ID_CTRL_PICTURE_8					      = 8;
-p.ID_CTRL_PICTURE_10					     = 12;
-p.ID_CTRL_BUTTON_11					      = 11;
+p.ID_CTRL_PICTURE_13					     = 13;
+p.ID_CTRL_PICTURE_12					     = 12;
 p.ID_CTRL_BUTTON_10					      = 10;
