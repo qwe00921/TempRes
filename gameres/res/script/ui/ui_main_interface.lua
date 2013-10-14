d3e1a5429ef91e1380c6e14287feffce70f62341
sgkqk <kqk@qq.com>
2013-10-14 15:44:11 +0800

    main_interface.xui添加控制菜单隐藏的按钮

--- gameres/res/script/ui/ui_main_interface.lua
+++ gameres/res/script/ui/ui_main_interface.lua
@@ -10,3 +10,4 @@ p.ID_CTRL_MAIN_LIST_ACT				   = 12;	--活动列表
 p.ID_CTRL_MAIN_CUSTOMIZE_LEVEL		= 27;
 p.ID_CTRL_MAIN_BUTTON_GIFT			 = 24;	--礼物按钮
 p.ID_CTRL_MAIN_PICTURE_BG				 = 1;	--背景
+p.ID_CTRL_MIAN_BUTTON_DOWN			 = 7;	--按钮（控制目录隐藏）
