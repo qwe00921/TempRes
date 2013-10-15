c43fe787ce279eb3bb64a1f1e0fc91216c1b7018
sgkqk <kqk@qq.com>
2013-10-15 15:09:25 +0800

    任务界面ui上传

---
+++ gameres/res/script/ui/ui_quest_main.lua
@@ -0,0 +1,15 @@
+--------------------------------------------------------------
+-- FileName:  quest_main.lua
+-- author: 此文件為自動生成，沒有作者。有問題請 @郭浩
+--------------------------------------------------------------
+ui_quest_main = {};
+local p =  ui_quest_main;
+p.ID_CTRL_QUEST_BUTTON_STARTGAME		= 12;
+p.ID_CTRL_QUEST_LIST_QUESTLIST		= 11;	--任务列表
+p.ID_CTRL_QUEST_BUTTON_BACK			= 10;	--返回按钮
+p.ID_CTRL_QUEST_PICTURE_HEAD			= 9;
+p.ID_CTRL_QUEST_TEXT_LEVELNAME		= 8;	--文字（关卡名称）
+p.ID_CTRL_QUEST_PICTURE_LEVELNAME		= 5;	--关卡名字
+p.ID_CTRL_QUEST_BUTTON_GASHAPON		= 4;	--扭蛋商店按钮
+p.ID_CTRL_QUEST_PICTURE_BG2			= 2;	--关卡详细背景
+p.ID_CTRL_QUEST_PICTURE_BG			 = 1;	--80%透明度背景
---
+++ gameres/res/script/ui/ui_quest_questlist.lua
@@ -0,0 +1,12 @@
+--------------------------------------------------------------
+-- FileName:  quest_questlist.lua
+-- author: 此文件為自動生成，沒有作者。有問題請 @郭浩
+--------------------------------------------------------------
+ui_quest_questlist = {};
+local p =  ui_quest_questlist;
+p.ID_CTRL_QUEST_PICTURE_HARD			= 8;	--困难选项
+p.ID_CTRL_QUEST_PICTURE_NOMAL			= 7;	--一般选项
+p.ID_CTRL_QUEST_PICTURE_EASY			= 6;	--简单选项
+p.ID_CTRL_TEXT_3						        = 3;
+p.ID_CTRL_QUEST_PICTURE_2				 = 2;	--关卡介绍图片
+p.ID_CTRL_QUEST_PICTURE_BG			 = 5;	--任务选择图
