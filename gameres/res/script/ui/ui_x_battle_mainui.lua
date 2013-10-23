ff333359911b7de425a05bc48764f5bc1a1d6816
GuoHao <hubris6965@hotmail.com>
2013-10-23 23:56:20 +0800

    横版相关提交。

--- gameres/res/script/ui/ui_x_battle_mainui.lua
+++ gameres/res/script/ui/ui_x_battle_mainui.lua
@@ -4,9 +4,5 @@
 --------------------------------------------------------------
 ui_x_battle_mainui = {};
 local p =  ui_x_battle_mainui;
-p.ID_CTRL_PICTURE_39					     = 39;
-p.ID_CTRL_PICTURE_38					     = 38;
-p.ID_CTRL_PICTURE_36					     = 36;
-p.ID_CTRL_PICTURE_12					     = 12;
 p.ID_CTRL_BUTTON_11					      = 11;
-p.ID_CTRL_BUTTON_10					      = 10;
+p.ID_CTRL_PICTURE_12					     = 12;
