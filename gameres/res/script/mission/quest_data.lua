ccb44dd0bf58657f67e7317127f66e183cf7b755
肖明奎 <xiaomk7758@sian.com>
2013-11-06 15:28:27 +0800

    QUEST

--- gameres/res/script/mission/quest_data.lua
+++ gameres/res/script/mission/quest_data.lua
@@ -3,156 +3,177 @@ quest_data["Stage_101"] = {}
 quest_data["Stage_101"]["questBattleNum"] = 7;
 
 ----------------1-1
-quest_data["Stage_101"]["quest_10001"] = {}
-quest_data["Stage_101"]["quest_10001"]["name"] = "1-1 世界树的名字"
+quest_data["Stage_101"]["quest_101011"] = {}
+quest_data["Stage_101"]["quest_101011"]["name"] = "1-1 世界树的名字"
 
-quest_data["Stage_101"]["quest_10001"]["easy"] = {}
-quest_data["Stage_101"]["quest_10001"]["easy"]["evaluation"] = "0"
-quest_data["Stage_101"]["quest_10001"]["easy"]["times"] = "3"
-quest_data["Stage_101"]["quest_10001"]["easy"]["money"] = "X1000"
-quest_data["Stage_101"]["quest_10001"]["easy"]["exquest_data"] = "X1000"
+quest_data["Stage_101"]["quest_101011"]["easy"] = {}
+quest_data["Stage_101"]["quest_101011"]["easy"]["evaluation"] = "0"
+quest_data["Stage_101"]["quest_101011"]["easy"]["times"] = "5"
+quest_data["Stage_101"]["quest_101011"]["easy"]["money"] = "X1000"
+quest_data["Stage_101"]["quest_101011"]["easy"]["exp"] = "X1000"
+quest_data["Stage_101"]["quest_101011"]["easy"]["item"] = 1
 
-quest_data["Stage_101"]["quest_10001"]["normal"] = {}
-quest_data["Stage_101"]["quest_10001"]["normal"]["evaluation"] = "0"
-quest_data["Stage_101"]["quest_10001"]["normal"]["times"] = "1"
-quest_data["Stage_101"]["quest_10001"]["normal"]["money"] = "X2000"
-quest_data["Stage_101"]["quest_10001"]["normal"]["exquest_data"] = "X2000"
+quest_data["Stage_101"]["quest_101011"]["normal"] = {}
+quest_data["Stage_101"]["quest_101011"]["normal"]["evaluation"] = "0"
+quest_data["Stage_101"]["quest_101011"]["normal"]["times"] = "4"
+quest_data["Stage_101"]["quest_101011"]["normal"]["money"] = "X2000"
+quest_data["Stage_101"]["quest_101011"]["normal"]["exp"] = "X2000"
+quest_data["Stage_101"]["quest_101011"]["normal"]["item"] = 2
 
-quest_data["Stage_101"]["quest_10001"]["difficult"] = {}
-quest_data["Stage_101"]["quest_10001"]["difficult"]["evaluation"] = "0"
-quest_data["Stage_101"]["quest_10001"]["difficult"]["times"] = "2"
-quest_data["Stage_101"]["quest_10001"]["difficult"]["money"] = "X3000"
-quest_data["Stage_101"]["quest_10001"]["difficult"]["exquest_data"] = "X3000"
+quest_data["Stage_101"]["quest_101011"]["difficult"] = {}
+quest_data["Stage_101"]["quest_101011"]["difficult"]["evaluation"] = "0"
+quest_data["Stage_101"]["quest_101011"]["difficult"]["times"] = "3"
+quest_data["Stage_101"]["quest_101011"]["difficult"]["money"] = "X3000"
+quest_data["Stage_101"]["quest_101011"]["difficult"]["exp"] = "X3000"
+quest_data["Stage_101"]["quest_101011"]["difficult"]["item"] = 3
 
 
 ----------------1-2
-quest_data["Stage_101"]["quest_10002"] = {}
-quest_data["Stage_101"]["quest_10002"]["name"] = "1-2 世界树的名字"
+quest_data["Stage_101"]["quest_101012"] = {}
+quest_data["Stage_101"]["quest_101012"]["name"] = "1-2 世界树的名字"
 
-quest_data["Stage_101"]["quest_10002"]["easy"] = {}
-quest_data["Stage_101"]["quest_10002"]["easy"]["evaluation"] = "0"
-quest_data["Stage_101"]["quest_10002"]["easy"]["times"] = "3"
-quest_data["Stage_101"]["quest_10002"]["easy"]["money"] = "X1000"
-quest_data["Stage_101"]["quest_10002"]["easy"]["exquest_data"] = "X1000"
+quest_data["Stage_101"]["quest_101012"]["easy"] = {}
+quest_data["Stage_101"]["quest_101012"]["easy"]["evaluation"] = "0"
+quest_data["Stage_101"]["quest_101012"]["easy"]["times"] = "3"
+quest_data["Stage_101"]["quest_101012"]["easy"]["money"] = "X1000"
+quest_data["Stage_101"]["quest_101012"]["easy"]["exp"] = "X1000"
+quest_data["Stage_101"]["quest_101012"]["easy"]["item"] = 1
 
-quest_data["Stage_101"]["quest_10002"]["normal"] = {}
-quest_data["Stage_101"]["quest_10002"]["normal"]["evaluation"] = "0"
-quest_data["Stage_101"]["quest_10002"]["normal"]["times"] = "1"
-quest_data["Stage_101"]["quest_10002"]["normal"]["money"] = "X2000"
-quest_data["Stage_101"]["quest_10002"]["normal"]["exquest_data"] = "X2000"
+quest_data["Stage_101"]["quest_101012"]["normal"] = {}
+quest_data["Stage_101"]["quest_101012"]["normal"]["evaluation"] = "0"
+quest_data["Stage_101"]["quest_101012"]["normal"]["times"] = "1"
+quest_data["Stage_101"]["quest_101012"]["normal"]["money"] = "X2000"
+quest_data["Stage_101"]["quest_101012"]["normal"]["exp"] = "X2000"
+quest_data["Stage_101"]["quest_101012"]["normal"]["item"] = 2
 
-quest_data["Stage_101"]["quest_10002"]["difficult"] = {}
-quest_data["Stage_101"]["quest_10002"]["difficult"]["evaluation"] = "0"
-quest_data["Stage_101"]["quest_10002"]["difficult"]["times"] = "2"
-quest_data["Stage_101"]["quest_10002"]["difficult"]["money"] = "X3000"
-quest_data["Stage_101"]["quest_10002"]["difficult"]["exquest_data"] = "X3000"
+quest_data["Stage_101"]["quest_101012"]["difficult"] = {}
+quest_data["Stage_101"]["quest_101012"]["difficult"]["evaluation"] = "0"
+quest_data["Stage_101"]["quest_101012"]["difficult"]["times"] = "2"
+quest_data["Stage_101"]["quest_101012"]["difficult"]["money"] = "X3000"
+quest_data["Stage_101"]["quest_101012"]["difficult"]["exp"] = "X3000"
+quest_data["Stage_101"]["quest_101012"]["difficult"]["item"] = 3
 
 ----------------1-3
-quest_data["Stage_101"]["quest_10003"] = {}
-quest_data["Stage_101"]["quest_10003"]["name"] = "1-3 世界树的名字"
+quest_data["Stage_101"]["quest_101013"] = {}
+quest_data["Stage_101"]["quest_101013"]["name"] = "1-3 世界树的名字"
 
-quest_data["Stage_101"]["quest_10003"]["easy"] = {}
-quest_data["Stage_101"]["quest_10003"]["easy"]["evaluation"] = "0"
-quest_data["Stage_101"]["quest_10003"]["easy"]["times"] = "3"
-quest_data["Stage_101"]["quest_10003"]["easy"]["money"] = "X1000"
-quest_data["Stage_101"]["quest_10003"]["easy"]["exquest_data"] = "X1000"
+quest_data["Stage_101"]["quest_101013"]["easy"] = {}
+quest_data["Stage_101"]["quest_101013"]["easy"]["evaluation"] = "0"
+quest_data["Stage_101"]["quest_101013"]["easy"]["times"] = "3"
+quest_data["Stage_101"]["quest_101013"]["easy"]["money"] = "X1000"
+quest_data["Stage_101"]["quest_101013"]["easy"]["exp"] = "X1000"
+quest_data["Stage_101"]["quest_101013"]["easy"]["item"] = 1
 
-quest_data["Stage_101"]["quest_10003"]["normal"] = {}
-quest_data["Stage_101"]["quest_10003"]["normal"]["evaluation"] = "0"
-quest_data["Stage_101"]["quest_10003"]["normal"]["times"] = "1"
-quest_data["Stage_101"]["quest_10003"]["normal"]["money"] = "X2000"
-quest_data["Stage_101"]["quest_10003"]["normal"]["exquest_data"] = "X2000"
+quest_data["Stage_101"]["quest_101013"]["normal"] = {}
+quest_data["Stage_101"]["quest_101013"]["normal"]["evaluation"] = "0"
+quest_data["Stage_101"]["quest_101013"]["normal"]["times"] = "1"
+quest_data["Stage_101"]["quest_101013"]["normal"]["money"] = "X2000"
+quest_data["Stage_101"]["quest_101013"]["normal"]["exp"] = "X2000"
+quest_data["Stage_101"]["quest_101013"]["normal"]["item"] = 2
 
-quest_data["Stage_101"]["quest_10003"]["difficult"] = {}
-quest_data["Stage_101"]["quest_10003"]["difficult"]["evaluation"] = "0"
-quest_data["Stage_101"]["quest_10003"]["difficult"]["times"] = "2"
-quest_data["Stage_101"]["quest_10003"]["difficult"]["money"] = "X3000"
-quest_data["Stage_101"]["quest_10003"]["difficult"]["exquest_data"] = "X3000"
+quest_data["Stage_101"]["quest_101013"]["difficult"] = {}
+quest_data["Stage_101"]["quest_101013"]["difficult"]["evaluation"] = "0"
+quest_data["Stage_101"]["quest_101013"]["difficult"]["times"] = "2"
+quest_data["Stage_101"]["quest_101013"]["difficult"]["money"] = "X3000"
+quest_data["Stage_101"]["quest_101013"]["difficult"]["exp"] = "X3000"
+quest_data["Stage_101"]["quest_101013"]["difficult"]["item"] = 3
 
 ----------------1-4
-quest_data["Stage_101"]["quest_10004"] = {}
-quest_data["Stage_101"]["quest_10004"]["name"] = "1-4 世界树的名字"
+quest_data["Stage_101"]["quest_101014"] = {}
+quest_data["Stage_101"]["quest_101014"]["name"] = "1-4 世界树的名字"
 
-quest_data["Stage_101"]["quest_10004"]["easy"] = {}
-quest_data["Stage_101"]["quest_10004"]["easy"]["evaluation"] = "0"
-quest_data["Stage_101"]["quest_10004"]["easy"]["times"] = "3"
-quest_data["Stage_101"]["quest_10004"]["easy"]["money"] = "X1000"
-quest_data["Stage_101"]["quest_10004"]["easy"]["exquest_data"] = "X1000"
+quest_data["Stage_101"]["quest_101014"]["easy"] = {}
+quest_data["Stage_101"]["quest_101014"]["easy"]["evaluation"] = "0"
+quest_data["Stage_101"]["quest_101014"]["easy"]["times"] = "3"
+quest_data["Stage_101"]["quest_101014"]["easy"]["money"] = "X1000"
+quest_data["Stage_101"]["quest_101014"]["easy"]["exp"] = "X1000"
+quest_data["Stage_101"]["quest_101014"]["easy"]["item"] = 1
 
-quest_data["Stage_101"]["quest_10004"]["normal"] = {}
-quest_data["Stage_101"]["quest_10004"]["normal"]["evaluation"] = "0"
-quest_data["Stage_101"]["quest_10004"]["normal"]["times"] = "1"
-quest_data["Stage_101"]["quest_10004"]["normal"]["money"] = "X2000"
-quest_data["Stage_101"]["quest_10004"]["normal"]["exquest_data"] = "X2000"
+quest_data["Stage_101"]["quest_101014"]["normal"] = {}
+quest_data["Stage_101"]["quest_101014"]["normal"]["evaluation"] = "0"
+quest_data["Stage_101"]["quest_101014"]["normal"]["times"] = "1"
+quest_data["Stage_101"]["quest_101014"]["normal"]["money"] = "X2000"
+quest_data["Stage_101"]["quest_101014"]["normal"]["exp"] = "X2000"
+quest_data["Stage_101"]["quest_101014"]["normal"]["item"] = 2
 
-quest_data["Stage_101"]["quest_10004"]["difficult"] = {}
-quest_data["Stage_101"]["quest_10004"]["difficult"]["evaluation"] = "0"
-quest_data["Stage_101"]["quest_10004"]["difficult"]["times"] = "2"
-quest_data["Stage_101"]["quest_10004"]["difficult"]["money"] = "X3000"
-quest_data["Stage_101"]["quest_10004"]["difficult"]["exquest_data"] = "X3000"
+quest_data["Stage_101"]["quest_101014"]["difficult"] = {}
+quest_data["Stage_101"]["quest_101014"]["difficult"]["evaluation"] = "0"
+quest_data["Stage_101"]["quest_101014"]["difficult"]["times"] = "2"
+quest_data["Stage_101"]["quest_101014"]["difficult"]["money"] = "X3000"
+quest_data["Stage_101"]["quest_101014"]["difficult"]["exp"] = "X3000"
+quest_data["Stage_101"]["quest_101014"]["difficult"]["item"] = 3
 
 ----------------1-5
-quest_data["Stage_101"]["quest_10005"] = {}
-quest_data["Stage_101"]["quest_10005"]["name"] = "1-5 世界树的名字"
+quest_data["Stage_101"]["quest_101015"] = {}
+quest_data["Stage_101"]["quest_101015"]["name"] = "1-5 世界树的名字"
 
-quest_data["Stage_101"]["quest_10005"]["easy"] = {}
-quest_data["Stage_101"]["quest_10005"]["easy"]["evaluation"] = "0"
-quest_data["Stage_101"]["quest_10005"]["easy"]["times"] = "3"
-quest_data["Stage_101"]["quest_10005"]["easy"]["money"] = "X1000"
-quest_data["Stage_101"]["quest_10005"]["easy"]["exquest_data"] = "X1000"
+quest_data["Stage_101"]["quest_101015"]["easy"] = {}
+quest_data["Stage_101"]["quest_101015"]["easy"]["evaluation"] = "0"
+quest_data["Stage_101"]["quest_101015"]["easy"]["times"] = "3"
+quest_data["Stage_101"]["quest_101015"]["easy"]["money"] = "X1000"
+quest_data["Stage_101"]["quest_101015"]["easy"]["exp"] = "X1000"
+quest_data["Stage_101"]["quest_101015"]["easy"]["item"] = 1
 
-quest_data["Stage_101"]["quest_10005"]["normal"] = {}
-quest_data["Stage_101"]["quest_10005"]["normal"]["evaluation"] = "0"
-quest_data["Stage_101"]["quest_10005"]["normal"]["times"] = "1"
-quest_data["Stage_101"]["quest_10005"]["normal"]["money"] = "X2000"
-quest_data["Stage_101"]["quest_10005"]["normal"]["exquest_data"] = "X2000"
+quest_data["Stage_101"]["quest_101015"]["normal"] = {}
+quest_data["Stage_101"]["quest_101015"]["normal"]["evaluation"] = "0"
+quest_data["Stage_101"]["quest_101015"]["normal"]["times"] = "1"
+quest_data["Stage_101"]["quest_101015"]["normal"]["money"] = "X2000"
+quest_data["Stage_101"]["quest_101015"]["normal"]["exp"] = "X2000"
+quest_data["Stage_101"]["quest_101015"]["normal"]["item"] = 2
 
-quest_data["Stage_101"]["quest_10005"]["difficult"] = {}
-quest_data["Stage_101"]["quest_10005"]["difficult"]["evaluation"] = "0"
-quest_data["Stage_101"]["quest_10005"]["difficult"]["times"] = "2"
-quest_data["Stage_101"]["quest_10005"]["difficult"]["money"] = "X3000"
-quest_data["Stage_101"]["quest_10005"]["difficult"]["exquest_data"] = "X3000"
+quest_data["Stage_101"]["quest_101015"]["difficult"] = {}
+quest_data["Stage_101"]["quest_101015"]["difficult"]["evaluation"] = "0"
+quest_data["Stage_101"]["quest_101015"]["difficult"]["times"] = "2"
+quest_data["Stage_101"]["quest_101015"]["difficult"]["money"] = "X3000"
+quest_data["Stage_101"]["quest_101015"]["difficult"]["exp"] = "X3000"
+quest_data["Stage_101"]["quest_101015"]["difficult"]["item"] = 3
 
 ----------------1-6
-quest_data["Stage_101"]["quest_10006"] = {}
-quest_data["Stage_101"]["quest_10006"]["name"] = "1-6 世界树的名字"
+quest_data["Stage_101"]["quest_101016"] = {}
+quest_data["Stage_101"]["quest_101016"]["name"] = "1-6 世界树的名字"
 
-quest_data["Stage_101"]["quest_10006"]["easy"] = {}
-quest_data["Stage_101"]["quest_10006"]["easy"]["evaluation"] = "0"
-quest_data["Stage_101"]["quest_10006"]["easy"]["times"] = "3"
-quest_data["Stage_101"]["quest_10006"]["easy"]["money"] = "X1000"
-quest_data["Stage_101"]["quest_10006"]["easy"]["exquest_data"] = "X1000"
+quest_data["Stage_101"]["quest_101016"]["easy"] = {}
+quest_data["Stage_101"]["quest_101016"]["easy"]["evaluation"] = "0"
+quest_data["Stage_101"]["quest_101016"]["easy"]["times"] = "3"
+quest_data["Stage_101"]["quest_101016"]["easy"]["money"] = "X1000"
+quest_data["Stage_101"]["quest_101016"]["easy"]["exp"] = "X1000"
+quest_data["Stage_101"]["quest_101016"]["easy"]["item"] = 1
 
-quest_data["Stage_101"]["quest_10006"]["normal"] = {}
-quest_data["Stage_101"]["quest_10006"]["normal"]["evaluation"] = "0"
-quest_data["Stage_101"]["quest_10006"]["normal"]["times"] = "1"
-quest_data["Stage_101"]["quest_10006"]["normal"]["money"] = "X2000"
-quest_data["Stage_101"]["quest_10006"]["normal"]["exquest_data"] = "X2000"
+quest_data["Stage_101"]["quest_101016"]["normal"] = {}
+quest_data["Stage_101"]["quest_101016"]["normal"]["evaluation"] = "0"
+quest_data["Stage_101"]["quest_101016"]["normal"]["times"] = "1"
+quest_data["Stage_101"]["quest_101016"]["normal"]["money"] = "X2000"
+quest_data["Stage_101"]["quest_101016"]["normal"]["exp"] = "X2000"
+quest_data["Stage_101"]["quest_101016"]["normal"]["item"] = 2
 
-quest_data["Stage_101"]["quest_10006"]["difficult"] = {}
-quest_data["Stage_101"]["quest_10006"]["difficult"]["evaluation"] = "0"
-quest_data["Stage_101"]["quest_10006"]["difficult"]["times"] = "2"
-quest_data["Stage_101"]["quest_10006"]["difficult"]["money"] = "X3000"
-quest_data["Stage_101"]["quest_10006"]["difficult"]["exquest_data"] = "X3000"
+quest_data["Stage_101"]["quest_101016"]["difficult"] = {}
+quest_data["Stage_101"]["quest_101016"]["difficult"]["evaluation"] = "0"
+quest_data["Stage_101"]["quest_101016"]["difficult"]["times"] = "2"
+quest_data["Stage_101"]["quest_101016"]["difficult"]["money"] = "X3000"
+quest_data["Stage_101"]["quest_101016"]["difficult"]["exp"] = "X3000"
+quest_data["Stage_101"]["quest_101016"]["difficult"]["item"] = 3
 
 ----------------1-7
-quest_data["Stage_101"]["quest_10007"] = {}
-quest_data["Stage_101"]["quest_10007"]["name"] = "1-7 世界树的名字"
+quest_data["Stage_101"]["quest_101017"] = {}
+quest_data["Stage_101"]["quest_101017"]["name"] = "1-7 世界树的名字"
 
-quest_data["Stage_101"]["quest_10007"]["easy"] = {}
-quest_data["Stage_101"]["quest_10007"]["easy"]["evaluation"] = "0"
-quest_data["Stage_101"]["quest_10007"]["easy"]["times"] = "3"
-quest_data["Stage_101"]["quest_10007"]["easy"]["money"] = "X1000"
-quest_data["Stage_101"]["quest_10007"]["easy"]["exquest_data"] = "X1000"
+quest_data["Stage_101"]["quest_101017"]["easy"] = {}
+quest_data["Stage_101"]["quest_101017"]["easy"]["evaluation"] = "0"
+quest_data["Stage_101"]["quest_101017"]["easy"]["times"] = "3"
+quest_data["Stage_101"]["quest_101017"]["easy"]["money"] = "X1000"
+quest_data["Stage_101"]["quest_101017"]["easy"]["exp"] = "X1000"
+quest_data["Stage_101"]["quest_101017"]["easy"]["item"] = 1
 
-quest_data["Stage_101"]["quest_10007"]["normal"] = {}
-quest_data["Stage_101"]["quest_10007"]["normal"]["evaluation"] = "0"
-quest_data["Stage_101"]["quest_10007"]["normal"]["times"] = "1"
-quest_data["Stage_101"]["quest_10007"]["normal"]["money"] = "X2000"
-quest_data["Stage_101"]["quest_10007"]["normal"]["exquest_data"] = "X2000"
+quest_data["Stage_101"]["quest_101017"]["normal"] = {}
+quest_data["Stage_101"]["quest_101017"]["normal"]["evaluation"] = "0"
+quest_data["Stage_101"]["quest_101017"]["normal"]["times"] = "1"
+quest_data["Stage_101"]["quest_101017"]["normal"]["money"] = "X2000"
+quest_data["Stage_101"]["quest_101017"]["normal"]["exp"] = "X2000"
+quest_data["Stage_101"]["quest_101017"]["normal"]["item"] = 2
 
-quest_data["Stage_101"]["quest_10007"]["difficult"] = {}
-quest_data["Stage_101"]["quest_10007"]["difficult"]["evaluation"] = "0"
-quest_data["Stage_101"]["quest_10007"]["difficult"]["times"] = "2"
-quest_data["Stage_101"]["quest_10007"]["difficult"]["money"] = "X3000"
-quest_data["Stage_101"]["quest_10007"]["difficult"]["exquest_data"] = "X3000"
+quest_data["Stage_101"]["quest_101017"]["difficult"] = {}
+quest_data["Stage_101"]["quest_101017"]["difficult"]["evaluation"] = "0"
+quest_data["Stage_101"]["quest_101017"]["difficult"]["times"] = "2"
+quest_data["Stage_101"]["quest_101017"]["difficult"]["money"] = "X3000"
+quest_data["Stage_101"]["quest_101017"]["difficult"]["exp"] = "X3000"
+quest_data["Stage_101"]["quest_101017"]["difficult"]["item"] = 3
