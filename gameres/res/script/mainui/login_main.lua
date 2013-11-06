c38d90243310336abe1b8adf47d3b30ca9fa3291
crj <11111111111111>
2013-11-06 17:30:27 +0800

    登陆主界面

--- gameres/res/script/mainui/login_main.lua
+++ gameres/res/script/mainui/login_main.lua
@@ -33,10 +33,10 @@ function p.ShowUI()
 	
 	p.layer = layer;
 
-	login_ui.ShowUI(); --测试战斗要关掉 @郭浩
+	--login_ui.ShowUI(); --测试战斗要关掉 @郭浩
 	--login_ui.ShowUI();
 	--p.InitExp();
-	--p.SetDelegate();
+	p.SetDelegate();
 	
 end
 
