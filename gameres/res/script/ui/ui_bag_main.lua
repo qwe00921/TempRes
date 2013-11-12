00d5d3a27a6a9d29742326625cf81b12d1da6e41
sgkqk <kqk@qq.com>
2013-11-12 15:23:33 +0800

    背包系统xui及替代资源

---
+++ gameres/res/script/ui/ui_bag_main.lua
@@ -0,0 +1,18 @@
+--------------------------------------------------------------
+-- FileName:  bag_main.lua
+-- author: 此文件為自動生成，沒有作者。有問題請 @郭浩
+--------------------------------------------------------------
+ui_bag_main = {};
+local p =  ui_bag_main;
+p.ID_CTRL_BUTTON_15					      = 15;
+p.ID_CTRL_TEXT_ITEMINFO				   = 14;
+p.ID_CTRL_TEXT_ITEMNUM				    = 13;
+p.ID_CTRL_PICTURE_ITEMNAME			 = 12;
+p.ID_CTRL_PICTURE_INFOBG				  = 11;
+p.ID_CTRL_VERTICAL_LIST_10			 = 10;
+p.ID_CTRL_PICTURE_LISTBG				  = 9;
+p.ID_CTRL_BUTTON_CLEAR				    = 8;
+p.ID_CTRL_PICTURE_NUM					    = 1;
+p.ID_CTRL_PICTURE_TITLE				   = 2;
+p.ID_CTRL_BUTTON_BACK					    = 3;
+p.ID_CTRL_PICTURE_BGTITLE				 = 4;
