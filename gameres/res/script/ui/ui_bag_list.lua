205a577f15c8c5623ed2d8d7ac6a122badb644e2
sgkqk <kqk@qq.com>
2013-11-13 19:47:06 +0800

    背包列表xui修改

--- gameres/res/script/ui/ui_bag_list.lua
+++ gameres/res/script/ui/ui_bag_list.lua
@@ -4,6 +4,15 @@
 --------------------------------------------------------------
 ui_bag_list = {};
 local p =  ui_bag_list;
+p.ID_CTRL_TEXT_14						       = 14;	--道具名称
+p.ID_CTRL_TEXT_13						       = 13;	--道具名称
+p.ID_CTRL_PICTURE_12					     = 12;	--物品图片
+p.ID_CTRL_PICTURE_11					     = 11;	--物品图片
+p.ID_CTRL_BUTTON_10					      = 10;	--道具列表背景
+p.ID_CTRL_BUTTON_9					       = 9;	--道具列表背景
+p.ID_CTRL_TEXT_5						        = 5;	--道具名称
+p.ID_CTRL_PICTURE_4					      = 4;	--物品图片
 p.ID_CTRL_TEXT_ITEMNAME				   = 3;	--道具名称
 p.ID_CTRL_PICTURE_PICITEM				 = 2;	--物品图片
 p.ID_CTRL_BUTTON_ITEMBG				   = 1;	--道具列表背景
+p.ID_CTRL_BUTTON_6					       = 6;	--道具列表背景
