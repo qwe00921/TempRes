4e87c30425cab2e9aed9572620a9b798cb8c909b
crj <11111111111111>
2013-11-13 21:25:14 +0800

    召唤兽系统客户端UI

---
+++ gameres/res/script/ui/ui_beast_main_list.lua
@@ -0,0 +1,23 @@
+--------------------------------------------------------------
+-- FileName:  beast_main_list.lua
+-- author: 此文件為自動生成，沒有作者。有問題請 @郭浩
+--------------------------------------------------------------
+ui_beast_main_list = {};
+local p =  ui_beast_main_list;
+p.ID_CTRL_PICTURE_17					     = 27;
+p.ID_CTRL_PICTURE_16					     = 26;
+p.ID_CTRL_PICTURE_SKILL				   = 25;
+p.ID_CTRL_BUTTON_FIGHT				    = 24;
+p.ID_CTRL_BUTTON_INCUBATE				 = 23;
+p.ID_CTRL_TEXT_22						       = 22;
+p.ID_CTRL_TEXT_ATTACK2				    = 21;
+p.ID_CTRL_TEXT_HP2					       = 20;
+p.ID_CTRL_TEXT_MANA					      = 19;
+p.ID_CTRL_TEXT_ATTACK					    = 18;
+p.ID_CTRL_TEXT_HP						       = 17;
+p.ID_CTRL_TEXT_ELEMENT				    = 16;
+p.ID_CTRL_PICTURE_ELEMENT				 = 15;
+p.ID_CTRL_TEXT_14						       = 14;
+p.ID_CTRL_TEXT_12						       = 12;
+p.ID_CTRL_PICTURE_BEAST				   = 11;
+p.ID_CTRL_PICTURE_LISTBG				  = 10;
