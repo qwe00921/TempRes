657072ebcb372dba608330f14bb58952a474a199
sgkqk <kqk@qq.com>
2013-11-15 14:52:52 +0800

    bag_list.xui更新

--- gameres/res/script/ui/ui_bag_list.lua
+++ gameres/res/script/ui/ui_bag_list.lua
@@ -4,10 +4,10 @@
 --------------------------------------------------------------
 ui_bag_list = {};
 local p =  ui_bag_list;
-p.ID_CTRL_PICTURE_EQUIP4				  = 71;
-p.ID_CTRL_PICTURE_EQUIP3				  = 70;
-p.ID_CTRL_PICTURE_EQUIP2				  = 69;
-p.ID_CTRL_PICTURE_EQUIP1				  = 64;
+p.ID_CTRL_PICTURE_EQUIP1				  = 26;
+p.ID_CTRL_PICTURE_EQUIP4				  = 29;	--装备标志4
+p.ID_CTRL_PICTURE_EQUIP3				  = 28;	--装备标志3
+p.ID_CTRL_PICTURE_EQUIP2				  = 27;	--装备标志2
 p.ID_CTRL_TEXT_ITEMNAME1				  = 5;	--道具名称
 p.ID_CTRL_TEXT_ITEMNAME2				  = 3;	--道具名称
 p.ID_CTRL_PICTURE_22					     = 22;
