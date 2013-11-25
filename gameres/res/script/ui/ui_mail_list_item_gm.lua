b8a19d7f19222277beddde71b7c9d372e7638d09
wjl <wjl@nd.cn>
2013-11-25 20:25:01 +0800

    邮件系统--客服列表界面,个人邮件详细界面

---
+++ gameres/res/script/ui/ui_mail_list_item_gm.lua
@@ -0,0 +1,9 @@
+--------------------------------------------------------------
+-- FileName:  mail_list_item_gm.lua
+-- author: 此文件為自動生成，沒有作者。有問題請 @郭浩
+--------------------------------------------------------------
+ui_mail_list_item_gm = {};
+local p =  ui_mail_list_item_gm;
+p.ID_CTRL_TEXT_TIME					      = 331;
+p.ID_CTRL_TEXT_TITLE					     = 330;
+p.ID_CTRL_PICTURE_BG					     = 298;
