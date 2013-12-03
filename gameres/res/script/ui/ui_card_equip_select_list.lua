3f65ecdb84cd0a66a58900a4f6cb1b36a17eb12f
wjl <wjl@nd.cn>
2013-12-03 23:08:26 +0800

    卡牌将备,穿戴及更换御载

--- gameres/res/script/ui/ui_card_equip_select_list.lua
+++ gameres/res/script/ui/ui_card_equip_select_list.lua
@@ -4,6 +4,7 @@
 --------------------------------------------------------------
 ui_card_equip_select_list = {};
 local p =  ui_card_equip_select_list;
+p.ID_CTRL_TEXT_HEAD					      = 48;
 p.ID_CTRL_TEXT_59						       = 59;
 p.ID_CTRL_TEXT_32						       = 39;
 p.ID_CTRL_TEXT_31						       = 38;
@@ -12,14 +13,11 @@ p.ID_CTRL_TEXT_28						       = 35;
 p.ID_CTRL_TEXT_27						       = 34;
 p.ID_CTRL_BUTTON_26					      = 33;
 p.ID_CTRL_TEXT_25						       = 32;
-p.ID_CTRL_BUTTON_SORT_BY				  = 30;
 p.ID_CTRL_VERTICAL_LIST_VIEW			= 31;
-p.ID_CTRL_BUTTON_PRO4					    = 29;
 p.ID_CTRL_BUTTON_PRO3					    = 28;
 p.ID_CTRL_BUTTON_PRO2					    = 26;
 p.ID_CTRL_BUTTON_PRO1					    = 25;
 p.ID_CTRL_BUTTON_ALL					     = 24;
 p.ID_CTRL_PICTURE_LIST_BG				 = 23;
 p.ID_CTRL_BUTTON_RETURN				   = 22;
-p.ID_CTRL_PICTURE_HEAD				    = 21;
 p.ID_CTRL_PICTURE_BG					     = 27;
