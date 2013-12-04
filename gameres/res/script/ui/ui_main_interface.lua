0bce460cd82961ae7418e32dc99c0e7dfe163da7
sgkqk <kqk@qq.com>
2013-12-04 20:15:38 +0800

    主页美术资源提交 主页xui修改

--- gameres/res/script/ui/ui_main_interface.lua
+++ gameres/res/script/ui/ui_main_interface.lua
@@ -4,7 +4,8 @@
 --------------------------------------------------------------
 ui_main_interface = {};
 local p =  ui_main_interface;
-p.ID_CTRL_BUTTON_BG_BTN				   = 11;	--隐藏子界面按钮
+p.ID_CTRL_LIST_13						       = 14;
+p.ID_CTRL_PICTURE_LISTBG				  = 13;
 p.ID_CTRL_TEXT_MAIL_TIPS_NUM			= 10;	--邮件数量
 p.ID_CTRL_TEXT_ACTIVITY_TIPS_NUM		= 9;	--活动数量
 p.ID_CTRL_PICTURE_MAIL_TIPS_BG		= 8;	--邮件数量通知背景
@@ -15,3 +16,4 @@ p.ID_CTRL_BUTTON_ACTIVITY				 = 4;	--活动按钮
 p.ID_CTRL_BUTTON_MORE					    = 3;	--弹出按钮
 p.ID_CTRL_PICTURE_CHA					    = 12;	--首领星灵
 p.ID_CTRL_MAIN_PICTURE_BG				 = 1;	--背景
+p.ID_CTRL_BUTTON_BG_BTN				   = 11;	--隐藏子界面按钮
