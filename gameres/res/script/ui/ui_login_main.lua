72186acb7973367debc5fd82e8390221bf5594fe
sgkqk <kqk@qq.com>
2013-12-09 21:32:34 +0800

    登陆界面美术资源修改

--- gameres/res/script/ui/ui_login_main.lua
+++ gameres/res/script/ui/ui_login_main.lua
@@ -4,6 +4,7 @@
 --------------------------------------------------------------
 ui_login_main = {};
 local p =  ui_login_main;
+p.ID_CTRL_9SLICES_29					     = 29;
 p.ID_CTRL_LOGIN_BUTTON_STARTGAME		= 15;	--开始游戏按钮
 p.ID_CTRL_LOGIN_TEXT_SEVERSELECT		= 14;	--玩家服务器选择信息
 p.ID_CTRL_LOGIN_BUTTON_SEVERSELECT	= 13;	--玩家选择服务器按钮
@@ -11,4 +12,3 @@ p.ID_CTRL_LOGIN_TEXT_IDINPUT			= 11;	--玩家账号选择信息
 p.ID_CTRL_LOGIN_TXTSEVER				  = 9;	--提示文字（选择服务器）
 p.ID_CTRL_LOGIN__BUTTON_ID			 = 7;	--玩家选择账号按钮
 p.ID_CTRL_LOGIN_TEXT_ID				   = 2;	--提示文字（输入账号）
-p.ID_CTRL_LOGIN_MAIN_BG				   = 1;
