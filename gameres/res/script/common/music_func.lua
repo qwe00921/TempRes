30f2a8b5777e928cb953b97c68553487801e39a0
wjl <wjl@nd.cn>
2013-12-09 20:19:18 +0800

    背景音乐

--- gameres/res/script/common/music_func.lua
+++ gameres/res/script/common/music_func.lua
@@ -8,7 +8,7 @@
 --播放战斗音乐
 function PlayMusic_Battle()
 	StopBGMusic()
-	
+	--[[
 	if E_DEMO_VER == 1 then
         PlayBGMusicByName( "bgm_battle.mp3" );
     elseif E_DEMO_VER == 2 then
@@ -16,12 +16,15 @@ function PlayMusic_Battle()
     elseif E_DEMO_VER == 3 then
         PlayBGMusicByName( "bgm_battle_v2.mp3" );--@todo        
     end        
+	]]--
+	PlayBGMusicByName( "bgm_battle.mp3" );
 end
 
 --播放任务地图音乐
 function PlayMusic_Task()
     StopBGMusic()
     
+	--[[
     if E_DEMO_VER == 1 then
         PlayBGMusicByName( "bgm_task.mp3" );
     elseif E_DEMO_VER == 2 then        
@@ -29,6 +32,8 @@ function PlayMusic_Task()
     elseif E_DEMO_VER == 3 then    
         PlayBGMusicByName( "bgm_task.mp3" );
     end        
+	]]--
+	PlayBGMusicByName( "bgm_task.mp3" );
 end
 
 --播放主界面音乐
@@ -44,6 +49,19 @@ function PlayMusic_MainUI()
         --PlayBGMusicByName( "bgm_task.mp3" );        
     end        
     --]]
+	PlayBGMusicByName( "bgm_main.mp3" ); 
+end
+
+--播放登录界面音乐
+function PlayMusic_LoginUI()
+    StopBGMusic()
+	PlayBGMusicByName( "bgm_login.mp3" ); 
+end
+
+--播放商店界面音乐
+function PlayMusic_ShopUI()
+    StopBGMusic()
+	PlayBGMusicByName( "bgm_shop.mp3" ); 
 end
 
 --停止背景音乐
