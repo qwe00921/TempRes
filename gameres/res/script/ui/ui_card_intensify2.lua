f8f3bcc54647ba8378211976de58d7d788f21428
sgkqk <kqk@qq.com>
2013-12-17 11:19:42 +0800

    新增xui界面

---
+++ gameres/res/script/ui/ui_card_intensify2.lua
@@ -0,0 +1,21 @@
+--------------------------------------------------------------
+-- FileName:  card_intensify2.lua
+-- author: 此文件為自動生成，沒有作者。有問題請 @郭浩
+--------------------------------------------------------------
+ui_card_intensify2 = {};
+local p =  ui_card_intensify2;
+p.ID_CTRL_TEXT_101					       = 101;
+p.ID_CTRL_TEXT_25						       = 32;
+p.ID_CTRL_BUTTON_SORT_BY				  = 30;
+p.ID_CTRL_VERTICAL_LIST_VIEW			= 31;
+p.ID_CTRL_PICTURE_41					     = 41;
+p.ID_CTRL_BUTTON_RETURN				   = 22;
+p.ID_CTRL_PICTURE_HEAD				    = 21;
+p.ID_CTRL_9SLICES_42					     = 42;
+p.ID_CTRL_BUTTON_PRO5					    = 36;
+p.ID_CTRL_BUTTON_PRO4					    = 29;
+p.ID_CTRL_BUTTON_PRO3					    = 28;
+p.ID_CTRL_BUTTON_PRO2					    = 26;
+p.ID_CTRL_BUTTON_PRO1					    = 25;
+p.ID_CTRL_BUTTON_ALL					     = 24;
+p.ID_CTRL_PICTURE_BG					     = 27;
