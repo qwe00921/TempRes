3d5fb2dd24bb1e22b3f044bc368162bc643028d6
sgkqk <kqk@qq.com>
2013-12-29 20:40:17 +0800

    界面调整

--- gameres/res/script/ui/ui_card_select.lua
+++ gameres/res/script/ui/ui_card_select.lua
@@ -1,21 +1,21 @@
 --------------------------------------------------------------
--- FileName:  card_intensify2.lua
+-- FileName:  card_select.lua
 -- author: 此文件為自動生成，沒有作者。有問題請 @郭浩
 --------------------------------------------------------------
-ui_card_intensify2 = {};
-local p =  ui_card_intensify2;
-p.ID_CTRL_PICTURE_66					     = 66;
-p.ID_CTRL_TEXT_65						       = 65;
-p.ID_CTRL_PICTURE_64					     = 64;
-p.ID_CTRL_TEXT_25						       = 32;
+ui_card_select = {};
+local p =  ui_card_select;
+p.ID_CTRL_TEXT_94						       = 94;
+p.ID_CTRL_PICTURE_92					     = 92;
+p.ID_CTRL_TEXT_87						       = 87;
+p.ID_CTRL_PICTURE_86					     = 86;
+p.ID_CTRL_TEXT_CARD_NUM				   = 38;
 p.ID_CTRL_BUTTON_SORT_BY				  = 30;
 p.ID_CTRL_VERTICAL_LIST_VIEW			= 31;
-p.ID_CTRL_PICTURE_41					     = 41;
-p.ID_CTRL_BUTTON_RETURN				   = 22;
-p.ID_CTRL_BUTTON_PRO5					    = 36;
+p.ID_CTRL_BUTTON_PRO5					    = 15;
 p.ID_CTRL_BUTTON_PRO4					    = 29;
 p.ID_CTRL_BUTTON_PRO3					    = 28;
 p.ID_CTRL_BUTTON_PRO2					    = 26;
 p.ID_CTRL_BUTTON_PRO1					    = 25;
 p.ID_CTRL_BUTTON_ALL					     = 24;
-p.ID_CTRL_PICTURE_236					    = 236;
+p.ID_CTRL_BUTTON_RETURN				   = 22;
+p.ID_CTRL_PICTURE_91					     = 91;
