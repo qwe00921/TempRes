2f7ff9ae6dda117da1293791049655fd14279823
csd <csd@test.com>
2014-01-02 14:44:03 +0800

    状态机的管理

---
+++ gameres/res/script/w_battle/w_battle_PVEStaMachMgr.lua
@@ -0,0 +1,30 @@
+w_battle_PVEStaMachMgr = {}
+local p = w_battle_PVEStaMachMgr;
+
+p.StateMachineLst = {};
+p.id = 0;
+
+function p.init()
+	p.StateMachineLst = {};
+	p.id = 0;
+end;
+
+
+function p.addStateMachine(pStateMachine)
+	p.id = p.id + 1;	
+	p.StateMachineLst[p.id] = pStateMachine;
+	
+	return p.id;
+	
+end
+
+
+function p.delStateMachine(id)
+	p.StateMachineLst[id] = nil;
+end
+
+function p.getSataeMachine(id)
+	return p.StateMachineLst[id]
+	
+end
+
