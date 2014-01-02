6cef87034c4c05634bd73a68dc76b1d302b64a41
GuoHao <hubris6965@hotmail.com>
2014-01-02 20:07:34 +0800

    提交修改，列表适配。

--- gameres/res/script/ui/ui_quest_main_view.lua
+++ gameres/res/script/ui/ui_quest_main_view.lua
@@ -4,11 +4,9 @@
 --------------------------------------------------------------
 ui_quest_main_view = {};
 local p =  ui_quest_main_view;
-
 p.ID_CTRL_VERTICAL_LIST				   = 5;
 p.ID_CTRL_BTN_HARD					       = 9;
 p.ID_CTRL_TEXT_QUEST_NAME				 = 6;
-p.ID_CTRL_PIC_LIST_BG					    = 3;
 p.ID_CTRL_TEXT_RETURN					    = 48;
 p.ID_CTRL_BTN_TETURN					     = 2;
 p.ID_CTRL_9SLICES_12					     = 12;
