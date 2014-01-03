77764f06e65bcf57a8c3b324023fc0ab2ec6dcb3
sgkqk <kqk@qq.com>
2014-01-03 14:34:59 +0800

    物品选择界面

---
+++ gameres/res/script/ui/ui_pubulic_item_num.lua
@@ -0,0 +1,19 @@
+--------------------------------------------------------------
+-- FileName:  pubulic_item_num.lua
+-- author: 此文件為自動生成，沒有作者。有問題請 @郭浩
+--------------------------------------------------------------
+ui_pubulic_item_num = {};
+local p =  ui_pubulic_item_num;
+p.ID_CTRL_BUTTON_15					      = 15;
+p.ID_CTRL_9SLICES_14					     = 14;
+p.ID_CTRL_TEXT_13						       = 13;
+p.ID_CTRL_TEXT_ITEMNAME				   = 12;
+p.ID_CTRL_PICTURE_10					     = 10;
+p.ID_CTRL_PICTURE_9					      = 9;
+p.ID_CTRL_PICTURE_8					      = 8;
+p.ID_CTRL_PICTURE_6					      = 7;
+p.ID_CTRL_TEXT_6						        = 6;
+p.ID_CTRL_TEXT_43						       = 44;
+p.ID_CTRL_PICTURE_41					     = 42;
+p.ID_CTRL_BUTTON_RETURN				   = 22;
+p.ID_CTRL_PICTURE_42					     = 43;
