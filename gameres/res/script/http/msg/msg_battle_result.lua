1fd6d3d4d6d86a81dbde5212b114bcd46041d096
csd <csd@test.com>
2014-01-03 20:40:01 +0800

    优化

--- gameres/res/script/http/msg/msg_battle_result.lua
+++ gameres/res/script/http/msg/msg_battle_result.lua
@@ -26,9 +26,9 @@ end
 function p:Process()
 	if self.result == true then 
 		--w_battle_mgr.GetReuslt();
-		w_battle_mgr.QuitBattle();
-		dlg_userinfo.ShowUI();
-		stageMap_main.OpenWorldMap();
+		--w_battle_mgr.QuitBattle();
+		--dlg_userinfo.ShowUI();
+		--stageMap_main.OpenWorldMap();
 		local lResult = w_battle_mgr.GetReuslt();
 		if lResult == 1 then
 			WriteConWarning( "**MSG_BATTLE_RESULT  Success" );
