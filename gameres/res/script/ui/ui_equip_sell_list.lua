0bd8b564b210ca4826adab98155c5e605fac86ab
sgkqk <kqk@qq.com>
2014-01-08 15:55:37 +0800

    xui修改

---
+++ gameres/res/script/ui/ui_equip_sell_list.lua
@@ -0,0 +1,25 @@
+--------------------------------------------------------------
+-- FileName:  equip_sell_list.lua
+-- author: 此文件為自動生成，沒有作者。有問題請 @郭浩
+--------------------------------------------------------------
+ui_equip_sell_list = {};
+local p =  ui_equip_sell_list;
+p.ID_CTRL_BUTTON_28					      = 34;
+p.ID_CTRL_TEXT_27						       = 33;
+p.ID_CTRL_TEXT_26						       = 32;
+p.ID_CTRL_TEXT_24						       = 30;
+p.ID_CTRL_PICTURE_23					     = 29;
+p.ID_CTRL_TEXT_21						       = 27;
+p.ID_CTRL_BUTTON_20					      = 26;
+p.ID_CTRL_PICTURE_22					     = 28;
+p.ID_CTRL_9SLICES_19					     = 25;
+p.ID_CTRL_TEXT_15						       = 15;
+p.ID_CTRL_PICTURE_14					     = 14;
+p.ID_CTRL_TEXT_13						       = 13;
+p.ID_CTRL_TEXT_12						       = 12;
+p.ID_CTRL_BUTTON_11					      = 11;
+p.ID_CTRL_TEXT_43						       = 44;
+p.ID_CTRL_PICTURE_41					     = 42;
+p.ID_CTRL_VERTICAL_LIST_VIEW			= 31;
+p.ID_CTRL_BUTTON_RETURN				   = 22;
+p.ID_CTRL_PICTURE_42					     = 43;
