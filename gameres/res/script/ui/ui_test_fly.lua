7153a7a94e9d7f8aaa5c978bffb9699d837676e8
sgkqk <kqk@qq.com>
2014-01-08 16:32:01 +0800

    飞入飞出测试xui

---
+++ gameres/res/script/ui/ui_test_fly.lua
@@ -0,0 +1,9 @@
+--------------------------------------------------------------
+-- FileName:  test_fly.lua
+-- author: 此文件為自動生成，沒有作者。有問題請 @郭浩
+--------------------------------------------------------------
+ui_test_fly = {};
+local p =  ui_test_fly;
+p.ID_CTRL_LIST_RIGHT					     = 3;
+p.ID_CTRL_BUTTON_2					       = 2;
+p.ID_CTRL_PICTURE_TOLEFT				  = 1;
