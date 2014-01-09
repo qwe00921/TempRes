9e3764cb752be2db905da3dc5055f15ecefab37a
sgkqk <kqk@qq.com>
2014-01-09 13:55:42 +0800

    xui调整

---
+++ gameres/res/script/ui/ui_country_levelup_top.lua
@@ -0,0 +1,9 @@
+--------------------------------------------------------------
+-- FileName:  country_levelup_top.lua
+-- author: 此文件為自動生成，沒有作者。有問題請 @郭浩
+--------------------------------------------------------------
+ui_country_levelup_top = {};
+local p =  ui_country_levelup_top;
+p.ID_CTRL_SPRITE_16					      = 16;
+p.ID_CTRL_SPRITE_15					      = 15;
+p.ID_CTRL_PICTURE_14					     = 14;
