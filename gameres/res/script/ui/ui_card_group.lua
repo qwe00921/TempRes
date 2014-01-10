f4f963511757c458466408423380c27433bb610f
sgkqk <kqk@qq.com>
2014-01-10 14:27:44 +0800

    界面调整

--- gameres/res/script/ui/ui_card_group.lua
+++ gameres/res/script/ui/ui_card_group.lua
@@ -4,7 +4,6 @@
 --------------------------------------------------------------
 ui_card_group = {};
 local p =  ui_card_group;
-p.ID_CTRL_PICTURE_DRAG_BG				 = 1219;
 p.ID_CTRL_BUTTON_110					     = 110;
 p.ID_CTRL_PICTURE_15					     = 16;
 p.ID_CTRL_PICTURE_13					     = 14;
@@ -12,6 +11,7 @@ p.ID_CTRL_PICTURE_12					     = 13;
 p.ID_CTRL_BUTTON_RIGHT				    = 12;
 p.ID_CTRL_BUTTON_LEFT					    = 11;
 p.ID_CTRL_LIST_9						        = 10;
+p.ID_CTRL_PICTURE_DRAG_BG				 = 1219;
 p.ID_CTRL_TEXT_8						        = 9;
 p.ID_CTRL_PICTURE_7					      = 8;
 p.ID_CTRL_TEXT_38_DELETE				  = 38;
