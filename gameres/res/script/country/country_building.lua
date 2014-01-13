d405529b96cea67260deabe245dbe3e9714bcbd8
MK <xiaomk7758@sian.com>
2014-01-13 21:13:40 +0800

    村庄升级

--- gameres/res/script/country/country_building.lua
+++ gameres/res/script/country/country_building.lua
@@ -45,63 +45,7 @@ function p.ShowUI(countryInfo)
 	
 	p.Init()
 end
-
-function p.Init()
-	p.upNeedTime = GetLabel(p.layer, ui.ID_CTRL_TEXT_UP1);
-	p.upNeed = GetLabel(p.layer, ui.ID_CTRL_TEXT_UP2);
-	p.upNeedHome = GetLabel(p.layer, ui.ID_CTRL_TEXT_UP3);
-	p.buildLevel = GetLabel(p.layer, ui.ID_CTRL_TEXT_BUILD_LV);
-	
-	local typeId = p.getNowType()
-	p.getBuildInfo(typeId)
-	
-end
-
-function p.getBuildInfo(typeId)
-	local nowBuildLv = nil;
-	local upIng = nil;
-	for k,v in pairs(p.countryInfoT) do
-		if k == "B"..typeId then
-			nowBuildLv = tonumber(v.build_level);
-			upIng = v.is_upgrade
-		end
-	end
-	p.getBuildNeedTable(typeId,nowBuildLv)
-end
--- "build_type": 2,
--- "build_level": 1,
--- "is_upgrade": 0,
--- "upgrade_time": 0,
--- "upgrade_level": 0,
--- "update": 0
-function p.getBuildNeedTable(typeId,nowLevel)
-	local buildTable =  SelectRowList(T_BUILDING,"type",typeId);
-	if buildTable == nil then
-		WriteConErr("upbuildTable is nil ");
-	end
-	local desText = nil;
-	local moneyNeed = nil;
-	local soulNeed = nil;
-	local timeNeed = nil;
-	local homeLvNeed = nil;
-	local playLvNeed = nil;
-	for k,v in pairs(buildTable) do
-		if tonumber(v.level) == tonumber(nowLevel) then
-			desText = v.description;
-			moneyNeed = v.cost_money
-			soulNeed = v.cost_soul
-			timeNeed = v.upgrade_time;
-			homeLvNeed = v.house_level_limit;
-			playLvNeed = v.player_level_limit;
-		end
-	end
-
-	p.upNeedTime:SetText("建造需要时间:"..timeNeed);
-	p.upNeed:SetText("金币:"..moneyNeed.."  蓝魂:"..soulNeed);
-	p.upNeedHome:SetText("住宅:"..homeLvNeed);
-	p.buildLevel:SetText("LV"..nowLevel);
-end
-
+--初始化建筑
 function p.InitScrollList()
 	local posCtrller = GetImage( p.layer, ui.ID_CTRL_PICTURE_BUILD );
 	local bList = createNDUIScrollContainerExpand();
@@ -139,9 +83,19 @@ end
 function p.OnTouchList(uiNode,uiEventType,param)
 	local typeId = p.getNowType()
 	p.getBuildInfo(typeId)
-	
 end
 
+function p.Init()
+	p.upNeedTime = GetLabel(p.layer, ui.ID_CTRL_TEXT_UP1);
+	p.upNeed = GetLabel(p.layer, ui.ID_CTRL_TEXT_UP2);
+	p.upNeedHome = GetLabel(p.layer, ui.ID_CTRL_TEXT_UP3);
+	p.buildLevel = GetLabel(p.layer, ui.ID_CTRL_TEXT_BUILD_LV);
+	
+	local typeId = p.getNowType()
+	p.getBuildInfo(typeId)
+	
+end
+--获取当前建筑TYPE
 function p.getNowType()
 	local indexId = p.scrollList:GetCurrentIndex()
 	
@@ -152,35 +106,48 @@ function p.getNowType()
 	WriteConErr("indexId ==== "..indexId);
 	return indexId
 end
-
-function p.OnTouchImage(uiNode, uiEventType, param)
-	if IsClickEvent( uiEventType ) then
-		local id = uiNode:GetId();
-		if id == 0 then
-			WriteCon("**========produceBtn========**");
-			-- stageMap_main.OpenWorldMap();
-			-- PlayMusic_Task(1);
-
-			-- maininterface.HideUI();
-		-- end
-	-- end
-		elseif id == 1 then
-			WriteCon("**========equipBtn========**");
-		elseif id == 2 then
-			WriteCon("**========mergeBtn========**");
-		elseif id == 3 then
-			WriteCon("**========homeBtn========**");
-		elseif id == 4 then
-			WriteCon("**========storeBtn========**");
-		elseif id == 5 then
-			WriteCon("**========riverBtn========**");
-		elseif id == 6 then
-			WriteCon("**========fieldBtn========**");
-		elseif id == 7 then
-			WriteCon("**========mountainBtn========**");
-		elseif id == 8 then
-			WriteCon("**========treeBtn========**");
+function p.getBuildInfo(typeId)
+	local nowBuildLv = nil;
+	local upIng = nil;
+	for k,v in pairs(p.countryInfoT) do
+		if k == "B"..typeId then
+			nowBuildLv = tonumber(v.build_level);
+			upIng = tonumber(v.is_upgrade);
 		end
+	end
+	p.getBuildNeedTable(typeId,nowBuildLv,upIng)
+end
+
+function p.getBuildNeedTable(typeId,nowLevel,upIng)
+	local buildTable =  SelectRowList(T_BUILDING,"type",typeId);
+	if buildTable == nil then
+		WriteConErr("upbuildTable is nil ");
+	end
+	local desText = nil;
+	local moneyNeed = nil;
+	local soulNeed = nil;
+	local timeNeed = nil;
+	local homeLvNeed = nil;
+	local playLvNeed = nil;
+	for k,v in pairs(buildTable) do
+		if tonumber(v.level) == tonumber(nowLevel) then
+			desText = v.description;
+			moneyNeed = v.cost_money
+			soulNeed = v.cost_soul
+			timeNeed = v.upgrade_time;
+			homeLvNeed = v.house_level_limit;
+			playLvNeed = v.player_level_limit;
+		end
+	end
+	if upIng == 0 then
+		p.upNeedTime:SetText("建造需要时间:"..timeNeed);
+		p.upNeed:SetText("金币:"..moneyNeed.."  蓝魂:"..soulNeed);
+		p.upNeedHome:SetText("住宅:"..homeLvNeed);
+		p.buildLevel:SetText("LV"..nowLevel);
+	elseif upIng == 1 then
+		p.upNeed:SetText("升级中");
+		p.upNeedHome:SetText(" ");
+		p.buildLevel:SetText(" ");
 	end
 end
 
@@ -256,6 +223,38 @@ function p.uiBuildCallBack(backData)
 
 end
 
+function p.OnTouchImage(uiNode, uiEventType, param)
+	if IsClickEvent( uiEventType ) then
+		local id = uiNode:GetId();
+		if id == 0 then
+			WriteCon("**========produceBtn========**");
+			-- stageMap_main.OpenWorldMap();
+			-- PlayMusic_Task(1);
+
+			-- maininterface.HideUI();
+		-- end
+	-- end
+		elseif id == 1 then
+			WriteCon("**========equipBtn========**");
+		elseif id == 2 then
+			WriteCon("**========mergeBtn========**");
+		elseif id == 3 then
+			WriteCon("**========homeBtn========**");
+		elseif id == 4 then
+			WriteCon("**========storeBtn========**");
+		elseif id == 5 then
+			WriteCon("**========riverBtn========**");
+		elseif id == 6 then
+			WriteCon("**========fieldBtn========**");
+		elseif id == 7 then
+			WriteCon("**========mountainBtn========**");
+		elseif id == 8 then
+			WriteCon("**========treeBtn========**");
+		end
+	end
+end
+
+
 --隐藏UI
 function p.HideUI()
 	if p.layer ~= nil then
@@ -275,3 +274,9 @@ end
 function p.UIDisappear()
 	p.CloseUI();
 end
+-- "build_type": 2,
+-- "build_level": 1,
+-- "is_upgrade": 0,
+-- "upgrade_time": 0,
+-- "upgrade_level": 0,
+-- "update": 0
