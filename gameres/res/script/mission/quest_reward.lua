c659d621e9d1be459a5ff47f4170f16150b9cc64
MK <xiaomk7758@sian.com>
2014-01-15 18:30:51 +0800

    调整奖励界面

--- gameres/res/script/mission/quest_reward.lua
+++ gameres/res/script/mission/quest_reward.lua
@@ -8,6 +8,10 @@ local expMax = nil;
 local expNow = nil;
 
 function p.ShowUI(rewardData)
+	if rewardData == nil then
+		WriteConErr("rewardData error");
+	end
+	
 	dlg_userinfo.ShowUI();
 	expLeast = 0;
 	expMax = tonumber(msg_cache.msg_player.MaxExp);
@@ -58,38 +62,38 @@ function p.OnBtnClick(uiNode,uiEventType,param)
 end
 
 function p.ShowQuestRewardView(rewardData)
+	local missionId = tonumber(rewardData.mission_id);
 	--章节名
 	local chapterName = GetLabel(p.layer, ui.ID_CTRL_TEXT_CHAPTER);
+	local stageId = math.floor(missionId/1000)
+	local stageT = SelectRowInner(T_STAGE,"stage_id",stageId);
+	chapterName:SetText(stageT.stage_name);
 	--任务名
 	local missionName = GetLabel(p.layer, ui.ID_CTRL_TEXT_MISSION_NAME);
-	local missionId = tonumber(rewardData.Mission_id);
 	local missionTable = SelectRowInner(T_MISSION,"id",missionId);
-	missionName:SetText(missionTable.mission_name);
+	missionName:SetText(missionTable.name);
 	--金币
 	local missionMoney = GetLabel(p.layer, ui.ID_CTRL_TEXT_MONEY_V);
-	local moneyText = tonumber(rewardData.reward_money)
-	if moneyText == nil or moneyText == 0 then
-		moneyText = "0";
-	end
+	local moneyText = tonumber(rewardData.money) or "0"
 	missionMoney:SetText(tostring(moneyText));
 	--魂
-	local soulText = GetLabel(p.layer, ui.ID_CTRL_TEXT_SOUL_V);
+	local missionSoul = GetLabel(p.layer, ui.ID_CTRL_TEXT_SOUL_V);
+	local soulText = tonumber(rewardData.soul) or "0"
+	missionSoul:SetText(tostring(soulText));
 	--经验
 	local missionExp = GetLabel(p.layer, ui.ID_CTRL_TEXT_EXP_V);
-	local expReward = tonumber(rewardData.Reward_exp);
-	if expReward == nil or expReward == 0 then
-		expReward = 0;
-	end
-	missionExp:SetText(tostring(expReward));
+	local expText = tonumber(rewardData.exp) or "0"
+	missionExp:SetText(tostring(expText));
+	
 	--升级所需经验
 	local needExpText = GetLabel(p.layer, ui.ID_CTRL_TEXT_NEED_EXP_V);
-	local expUpNeed = expMax - expNow - expReward;
+	local expUpNeed = expMax - expNow - tonumber(expText);
 	needExpText:SetText(tostring(expUpNeed))
 	
 	local expBar = GetExp( p.layer, ui.ID_CTRL_EXP_EXP );
-	local expSstartNum = expNow;
-	local expEndNum = expNow + expReward;
-	expbar_move_effect.showEffect(expBar,expLeast,expMax,expSstartNum,expEndNum)
+	local expStartNum = expNow;
+	local expEndNum = expNow + tonumber(expText);
+	expbar_move_effect.showEffect(expBar,expLeast,expMax,expStartNum,expEndNum)
 	
 end
 
