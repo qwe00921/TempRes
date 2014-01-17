baf9c6f5d47b765383e058e840688077e86fc580
csd <csd@test.com>
2014-01-17 14:27:05 +0800

    申请战斗直接胜利

--- gameres/res/script/http/msg/msg_battle_result.lua
+++ gameres/res/script/http/msg/msg_battle_result.lua
@@ -9,7 +9,7 @@ function p:new()
 	o = {}
 	setmetatable( o, self );
     self.__index = self;
-    o:ctor();
+    o:ctor();	
 	return o;
 end
 
@@ -28,7 +28,9 @@ function p:Process()
 		--w_battle_mgr.QuitBattle();
 		--local lResult = w_battle_mgr.GetReuslt();
 		--if lResult == 1 then
+
 			quest_reward.ShowUI(self.Reward);
+
 		--	dlg_userinfo.ShowUI();
 		--	stageMap_main.OpenWorldMap();
 		--end;
