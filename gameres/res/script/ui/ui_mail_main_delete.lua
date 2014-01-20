3d9ad7ca8b0a0c0558d344241fed638465dd36a4
wjl <wjl@nd.cn>
2014-01-20 20:48:56 +0800

    邮件

---
+++ gameres/res/script/ui/ui_mail_main_delete.lua
@@ -0,0 +1,15 @@
+--------------------------------------------------------------
+-- FileName:  mail_main_delete.lua
+-- author: 此文件為自動生成，沒有作者。有問題請 @郭浩
+--------------------------------------------------------------
+ui_mail_main_delete = {};
+local p =  ui_mail_main_delete;
+p.ID_CTRL_PICTURE_26					     = 26;
+p.ID_CTRL_PICTURE_25					     = 25;
+p.ID_CTRL_BUTTON_NEXT_PAGE			 = 162;
+p.ID_CTRL_TEXT_PAGE_INFO				  = 161;
+p.ID_CTRL_BUTTON_PRE_PAGE				 = 160;
+p.ID_CTRL_BUTTON_DEL					     = 159;
+p.ID_CTRL_BUTTON_SELECT_ALL			= 158;
+p.ID_CTRL_PICTURE_76					     = 76;
+p.ID_CTRL_TEXT_164_DELETE				 = 164;
