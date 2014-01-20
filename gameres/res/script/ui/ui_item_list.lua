3f04251aa64268ed90a0496b3d9f884299f8eabd
sgkqk <kqk@qq.com>
2014-01-20 14:23:25 +0800

    xui调整

--- gameres/res/script/ui/ui_item_list.lua
+++ gameres/res/script/ui/ui_item_list.lua
@@ -27,4 +27,3 @@ p.ID_CTRL_TEXT_43						       = 44;
 p.ID_CTRL_PICTURE_41					     = 42;
 p.ID_CTRL_VERTICAL_LIST_VIEW			= 31;
 p.ID_CTRL_BUTTON_RETURN				   = 22;
-p.ID_CTRL_PICTURE_42					     = 43;
