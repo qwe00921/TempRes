7dc3070f2a1bbe8359f8dc5794843dcfe1d99141
sgkqk <kqk@qq.com>
2014-01-21 17:45:30 +0800

    xui调整

--- gameres/res/script/ui/ui_item_produce.lua
+++ gameres/res/script/ui/ui_item_produce.lua
@@ -4,6 +4,7 @@
 --------------------------------------------------------------
 ui_item_produce = {};
 local p =  ui_item_produce;
+p.ID_CTRL_TEXT_59						       = 61;
 p.ID_CTRL_TEXT_58						       = 60;
 p.ID_CTRL_TEXT_57						       = 59;
 p.ID_CTRL_TEXT_56						       = 58;
