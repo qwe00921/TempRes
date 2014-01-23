ebff68538558885b1bc5cfa287986d7e96c5288b
sgkqk <kqk@qq.com>
2014-01-24 00:34:53 +0800

    xui修改

--- gameres/res/script/ui/ui_quest_main_view.lua
+++ gameres/res/script/ui/ui_quest_main_view.lua
@@ -7,5 +7,5 @@ local p =  ui_quest_main_view;
 p.ID_CTRL_VERTICAL_LIST				   = 5;
 p.ID_CTRL_BTN_HARD					       = 9;
 p.ID_CTRL_TEXT_QUEST_NAME				 = 6;
-p.ID_CTRL_TEXT_RETURN					    = 48;
 p.ID_CTRL_BTN_TETURN					     = 2;
+p.ID_CTRL_PICTURE_6					      = 7;
