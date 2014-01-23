9d566cc4849a0772c14d5199838f02b4297ed2f0
crj <asdasdq>
2014-01-23 16:44:35 +0800

    【主界面】UI修改

--- gameres/res/script/ui/ui_main_menu.lua
+++ gameres/res/script/ui/ui_main_menu.lua
@@ -4,6 +4,20 @@
 --------------------------------------------------------------
 ui_main_menu = {};
 local p =  ui_main_menu;
+p.ID_CTRL_PICTURE_305					    = 305;
+p.ID_CTRL_PICTURE_175					    = 175;
+p.ID_CTRL_PICTURE_152					    = 152;
+p.ID_CTRL_PICTURE_151					    = 151;
+p.ID_CTRL_PICTURE_150					    = 150;
+p.ID_CTRL_PICTURE_149					    = 149;
+p.ID_CTRL_PICTURE_148					    = 148;
+p.ID_CTRL_PICTURE_147					    = 147;
+p.ID_CTRL_PICTURE_146					    = 146;
+p.ID_CTRL_PICTURE_145					    = 145;
+p.ID_CTRL_PICTURE_144					    = 144;
+p.ID_CTRL_PICTURE_143					    = 143;
+p.ID_CTRL_PICTURE_142					    = 142;
+p.ID_CTRL_COLOR_LABEL_140				 = 140;
 p.ID_CTRL_BUTTON_GASHAPON				 = 7;	--扭蛋按钮
 p.ID_CTRL_BUTTON_PVP					     = 6;	--竞技场按钮
 p.ID_CTRL_BUTTON_BAG					     = 5;	--背包按钮
@@ -11,3 +25,5 @@ p.ID_CTRL_BUTTON_CARD_GROUP			= 4;	--卡组按钮
 p.ID_CTRL_BUTTON_QUEST				    = 3;	--任务按钮
 p.ID_CTRL_BUTTON_HOME_PAGE			 = 2;	--主页按钮
 p.ID_CTRL_PICTURE_BG					     = 1;	--背景图片
+p.ID_CTRL_PICTURE_8					      = 8;
+p.ID_CTRL_PICTURE_141					    = 141;
