ae1c0ad4bb14d519310773abd0b6476d36a1850a
crj <asdasdq>
2014-01-23 10:50:14 +0800

    新手引导

--- gameres/res/script/rookie/rookie_main.lua
+++ gameres/res/script/rookie/rookie_main.lua
@@ -11,20 +11,19 @@ function p.getRookieStep(backData)
 		dlg_msgbox.ShowOK("错误提示",backData.message,nil,p.layer);
 		return;
 	elseif backData.result == true then
-		local stepId = tonumber(backData.user.Guide_id)
+		local stepId = tonumber(backData.user.Guide_id);
 		p.userData = backData.user;
 		if stepId == 0 then
 			maininterface.ShowUI(backData.user);
 		else
-			p.ShowLearningStep( stepId )
+			p.ShowLearningStep( stepId, 1 );
 		end
 	end
 end
 
-
 --进入新手引导
-function p.ShowLearningStep( step )
-	WriteConErr("rookie step == "..step);
+function p.ShowLearningStep( step, substep )
+	WriteConErr("rookie step = "..step .. " substep = " .. substep);
 
 	if step == 1 then
 		maininterface.ShowUI(p.userData);
@@ -58,7 +57,7 @@ function p.ShowLearningStep( step )
 end
 
 --点击高亮区域回调
-function p.MaskTouchCallBack( step )
+function p.MaskTouchCallBack( step, substep )
 	if step == 1 then
 		
 	elseif step == 2 then
@@ -91,6 +90,36 @@ function p.MaskTouchCallBack( step )
 end
 
 --获取高亮区域
-function p.GetHighLightRect( step )
-	return CCRectMake( 0, 0, 0, 0 );
+function p.GetHighLightRect( step, substep )
+	local rect =  CCRectMake( 0, 0, 0, 0 );
+	if step == 1 then
+		
+	elseif step == 2 then
+		
+	elseif step == 3 then
+		
+	elseif step == 4 then
+		
+	elseif step == 5 then
+		
+	elseif step == 6 then
+		
+	elseif step == 7 then
+		
+	elseif step == 8 then
+		
+	elseif step == 9 then
+		
+	elseif step == 10 then
+		
+	elseif step == 11 then
+		
+	elseif step == 12 then
+		
+	elseif step == 13 then
+		
+	elseif step == 14 then
+		
+	end
+	return rect;
 end
