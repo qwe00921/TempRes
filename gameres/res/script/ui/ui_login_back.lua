87cd0d546d16c1aa18a3d3f121a7f2b3fbfe33d9
GuoHao <hubris6965@hotmail.com>
2014-01-25 01:39:50 +0800

    更新背景

--- gameres/res/script/ui/ui_login_back.lua
+++ gameres/res/script/ui/ui_login_back.lua
@@ -4,7 +4,5 @@
 --------------------------------------------------------------
 ui_login_back = {};
 local p =  ui_login_back;
-p.ID_CTRL_PICTURE_3					      = 3;
 p.ID_CTRL_BUTTON_102					     = 102;
-p.ID_CTRL_LOGIN_BG					       = 1;
 p.ID_CTRL_BUTTON_4					       = 4;
