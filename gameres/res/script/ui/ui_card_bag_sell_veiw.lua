af357922d5713ac9a58fd41083c67710491e82d5
sgkqk <kqk@qq.com>
2014-02-10 17:39:48 +0800

    xui调整

--- gameres/res/script/ui/ui_card_bag_sell_veiw.lua
+++ gameres/res/script/ui/ui_card_bag_sell_veiw.lua
@@ -4,6 +4,7 @@
 --------------------------------------------------------------
 ui_card_bag_sell_veiw = {};
 local p =  ui_card_bag_sell_veiw;
+p.ID_CTRL_PICTURE_53					     = 53;
 p.ID_CTRL_TEXT_SELL_NUM				   = 95;
 p.ID_CTRL_TEXT_89						       = 93;
 p.ID_CTRL_TEXT_SELL_MONEY				 = 90;
@@ -11,5 +12,4 @@ p.ID_CTRL_PICTURE_MONEY				   = 89;
 p.ID_CTRL_TEXT_86						       = 88;
 p.ID_CTRL_BUTTON_SELL					    = 85;
 p.ID_CTRL_BUTTON_CLEAR				    = 84;
-p.ID_CTRL_PICTURE_BG					     = 83;
 p.ID_CTRL_PICTURE_20					     = 20;
