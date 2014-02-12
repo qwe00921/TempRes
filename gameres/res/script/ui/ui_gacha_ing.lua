72926547272ce0007591a925fa264f6d830f4617
crj <asdasdq>
2014-02-12 18:15:17 +0800

    扭蛋结果效果调整

--- gameres/res/script/ui/ui_gacha_ing.lua
+++ gameres/res/script/ui/ui_gacha_ing.lua
@@ -8,5 +8,4 @@ p.ID_CTRL_PICTURE_6					      = 6;
 p.ID_CTRL_PICTURE_CARD				    = 7;
 p.ID_CTRL_PICTURE_SCALE				   = 41;
 p.ID_CTRL_SPRITE_MAGICCIRCLE			= 10;
-p.ID_CTRL_PICTURE_11					     = 11;
 p.ID_CTRL_BUTTON_9					       = 9;
