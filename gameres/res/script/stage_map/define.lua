896b22bb9efbed779d8c778775556ac5aed2f079
MK <xiaomk7758@sian.com>
2014-02-13 13:19:12 +0800

    提交map2

--- gameres/res/script/stage_map/define.lua
+++ gameres/res/script/stage_map/define.lua
@@ -1,4 +1,5 @@
 DoFile("stage_map/stageMap_1.lua");
+DoFile("stage_map/stageMap_2.lua");
 DoFile("stage_map/stageMap_main.lua");
 
 
--- gameres/res/script/ui/ui_map2.lua
+++ gameres/res/script/ui/ui_map2.lua
@@ -4,22 +4,22 @@
 --------------------------------------------------------------
 ui_map2 = {};
 local p =  ui_map2;
-p.ID_CTRL_TEXT_CHAPTER7				   = 27;
-p.ID_CTRL_PICTURE_HEAD7				   = 28;
-p.ID_CTRL_BUTTON_CHAPTER7				 = 26;
 p.ID_CTRL_BUTTON_RETURN				   = 23;
+p.ID_CTRL_TEXT_CHAPTER7				   = 27;
 p.ID_CTRL_TEXT_CHAPTER6				   = 24;
 p.ID_CTRL_TEXT_CHAPTER5				   = 22;
 p.ID_CTRL_TEXT_CHAPTER4				   = 21;
 p.ID_CTRL_TEXT_CHAPTER3				   = 20;
 p.ID_CTRL_TEXT_CHAPTER2				   = 19;
 p.ID_CTRL_TEXT_CHAPTER1				   = 18;
+p.ID_CTRL_PICTURE_HEAD7				   = 28;
 p.ID_CTRL_PICTURE_HEAD6				   = 17;
 p.ID_CTRL_PICTURE_HEAD5				   = 14;
 p.ID_CTRL_PICTURE_HEAD4				   = 12;
 p.ID_CTRL_PICTURE_HEAD3				   = 11;
 p.ID_CTRL_PICTURE_HEAD2				   = 10;
 p.ID_CTRL_PICTURE_HEAD1				   = 9;
+p.ID_CTRL_BUTTON_CHAPTER7				 = 26;
 p.ID_CTRL_BUTTON_CHAPTER6				 = 6;
 p.ID_CTRL_BUTTON_CHAPTER5				 = 5;
 p.ID_CTRL_BUTTON_CHAPTER4				 = 4;
