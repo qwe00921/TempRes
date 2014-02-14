5482b2f773f70ce2376fff9d35202d70ca4bc272
wjl <wjl@nd.cn>
2014-02-14 15:08:34 +0800

    取消拖动设置

--- gameres/res/script/ui/ui_card_group.lua
+++ gameres/res/script/ui/ui_card_group.lua
@@ -5,12 +5,12 @@
 ui_card_group = {};
 local p =  ui_card_group;
 p.ID_CTRL_PICTURE_18					     = 18;
-p.ID_CTRL_PICTURE_17					     = 17;
+p.ID_CTRL_BUTTON_110					     = 17;
 p.ID_CTRL_PICTURE_16					     = 16;
 p.ID_CTRL_PICTURE_15					     = 15;
 p.ID_CTRL_PICTURE_14					     = 14;
 p.ID_CTRL_PICTURE_13					     = 13;
-p.ID_CTRL_BUTTON_110					     = 110;
+p.ID_CTRL_PICTURE_17					     = 110;
 p.ID_CTRL_BUTTON_RIGHT				    = 12;
 p.ID_CTRL_BUTTON_LEFT					    = 11;
 p.ID_CTRL_LIST_9						        = 10;
