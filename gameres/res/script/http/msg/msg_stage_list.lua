15b3fb6dae3bcbd8a76842f4cb4b1f84f5b3657b
MK <xiaomk7758@sian.com>
2014-02-15 19:43:06 +0800

    debug

--- gameres/res/script/http/msg/msg_stage_list.lua
+++ gameres/res/script/http/msg/msg_stage_list.lua
@@ -27,5 +27,19 @@ function p:Process()
 		stageMap_1.addAllStage(self);
 	elseif stageMap_main.openMapId == 2 then
 		stageMap_2.addAllStage(self);
+	elseif stageMap_main.openMapId == 3 then
+		stageMap_3.addAllStage(self);
+	elseif stageMap_main.openMapId == 4 then
+		stageMap_4.addAllStage(self);
+	elseif stageMap_main.openMapId == 5 then
+		stageMap_5.addAllStage(self);
+	elseif stageMap_main.openMapId == 6 then
+		stageMap_6.addAllStage(self);
+	elseif stageMap_main.openMapId == 7 then
+		stageMap_7.addAllStage(self);
+	elseif stageMap_main.openMapId == 8 then
+		stageMap_8.addAllStage(self);
+	elseif stageMap_main.openMapId == 9 then
+		stageMap_9.addAllStage(self);
 	end
 end
