4d437fbce047da58dd5e2a12dc23ac2e5938175c
sgkqk <kqk@qq.com>
2014-02-19 15:16:52 +0800

    xui调整

--- gameres/res/script/ui/ui_mail_write_mail.lua
+++ gameres/res/script/ui/ui_mail_write_mail.lua
@@ -4,7 +4,6 @@
 --------------------------------------------------------------
 ui_mail_write_mail = {};
 local p =  ui_mail_write_mail;
-p.ID_CTRL_TEXT_43						       = 43;
 p.ID_CTRL_PICTURE_32					     = 32;
 p.ID_CTRL_INPUT_CONTENT				   = 296;
 p.ID_CTRL_9SLICES_19					     = 19;
@@ -18,4 +17,3 @@ p.ID_CTRL_BUTTON_GO_BACK				  = 159;
 p.ID_CTRL_9SLICES_17					     = 17;
 p.ID_CTRL_BUTTON_SEND					    = 158;
 p.ID_CTRL_PICTURE_16					     = 16;
-p.ID_CTRL_PICTURE_245					    = 245;
