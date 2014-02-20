e53ee3bcca2cc400691f52569c626552c539ba00
csd <csd@test.com>
2014-02-20 20:11:48 +0800

    因普通攻击和技能攻击同时触发引起 战斗卡住的现象

--- gameres/res/script/ui/ui_card_group.lua
+++ gameres/res/script/ui/ui_card_group.lua
@@ -4,6 +4,8 @@
 --------------------------------------------------------------
 ui_card_group = {};
 local p =  ui_card_group;
+p.ID_CTRL_BUTTON_RIGHT				    = 20;
+p.ID_CTRL_BUTTON_LEFT					    = 19;
 p.ID_CTRL_PICTURE_18					     = 18;
 p.ID_CTRL_BUTTON_110					     = 17;
 p.ID_CTRL_PICTURE_16					     = 16;
@@ -11,8 +13,8 @@ p.ID_CTRL_PICTURE_15					     = 15;
 p.ID_CTRL_PICTURE_14					     = 14;
 p.ID_CTRL_PICTURE_13					     = 13;
 p.ID_CTRL_PICTURE_17					     = 110;
-p.ID_CTRL_BUTTON_RIGHT				    = 12;
-p.ID_CTRL_BUTTON_LEFT					    = 11;
+p.ID_CTRL_BUTTON_RIGHT2				   = 12;
+p.ID_CTRL_BUTTON_LEFT2				    = 11;
 p.ID_CTRL_LIST_9						        = 10;
 p.ID_CTRL_PICTURE_DRAG_BG				 = 1219;
 p.ID_CTRL_PICTURE_7					      = 8;
