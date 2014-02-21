3ff0cdc8979231868b32f64635928aeb9f2a7274
csd <csd@test.com>
2014-02-21 15:56:19 +0800

    修正：属性排序

--- gameres/res/script/card_box/card_intensify.lua
+++ gameres/res/script/card_box/card_intensify.lua
@@ -797,6 +797,9 @@ function p.sortByRule(sortType)
 			elseif sortType == CARD_BAG_SORT_BY_TIME then
 				WriteCon("========sort by Elementb");
 				table.sort(p.cardListByProf,p.sortByTimeb);
+			elseif sortType == CARD_BAG_SORT_BY_TYPE then
+				WriteCon("========sort by Elementb");
+				table.sort(p.cardListByProf,p.sortByTypeb);
 			end
 		else
 			p.sortByRuleNum = sortType;
@@ -809,6 +812,9 @@ function p.sortByRule(sortType)
 			elseif sortType == CARD_BAG_SORT_BY_TIME then
 				WriteCon("========sort by Element");
 				table.sort(p.cardListByProf,p.sortByTime);
+			elseif sortType == CARD_BAG_SORT_BY_TYPE then
+				WriteCon("========sort by Elementb");
+				table.sort(p.cardListByProf,p.sortByType);
 			end
 		end
 	end
@@ -832,13 +838,25 @@ end
 function p.sortByStarb(a,b)
 	return tonumber(a.Rare) > tonumber(b.Rare) or ( tonumber(a.Rare) == tonumber(b.Rare) and tonumber(a.CardID) < tonumber(b.CardID));
 end
---按属性排序
+--按时间排序
 function p.sortByTime(a,b)
 	return tonumber(a.element) < tonumber(b.element) or ( tonumber(a.element) == tonumber(b.element) and tonumber(a.CardID) < tonumber(b.CardID));
 end
---按属性排序2
+--按时间排序2
 function p.sortByTimeb(a,b)
 	return tonumber(a.element) > tonumber(b.element) or ( tonumber(a.element) == tonumber(b.element) and tonumber(a.CardID) < tonumber(b.CardID));
+end
+
+--按属性排序
+function p.sortByType(a,b)
+	--return tonumber(a.element) < tonumber(b.element);
+	return tonumber(a.element) > tonumber(b.element) or ( tonumber(a.element) == tonumber(b.element) and tonumber(a.CardID) < tonumber(b.CardID));
+end
+
+--按属性排序2
+function p.sortByTypeb(a,b)
+	--return tonumber(a.element) < tonumber(b.element);
+	return tonumber(a.element) < tonumber(b.element) or ( tonumber(a.element) == tonumber(b.element) and tonumber(a.CardID) < tonumber(b.CardID));
 end
 
 function p.CloseUI()
