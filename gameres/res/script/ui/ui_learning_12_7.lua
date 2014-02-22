d6ade1f84609c81ad417eee3ce6996bc1ea15c4b
肖明奎 <xiaomk7758@sian.com>
2014-02-22 21:33:57 +0800

    新手XUI

---
+++ gameres/res/script/ui/ui_learning_12_7.lua
@@ -0,0 +1,11 @@
+--------------------------------------------------------------
+-- FileName:  learning_12_7.lua
+-- author: 此文件為自動生成，沒有作者。有問題請 @郭浩
+--------------------------------------------------------------
+ui_learning_12_7 = {};
+local p =  ui_learning_12_7;
+p.ID_CTRL_BUTTON_CALLBACK_1			= 20;
+p.ID_CTRL_PICTURE_4					      = 4;
+p.ID_CTRL_PICTURE_3					      = 3;
+p.ID_CTRL_PICTURE_2					      = 2;
+p.ID_CTRL_PICTURE_1					      = 1;
---
+++ gameres/res/script/ui/ui_learning_7_3.lua
@@ -0,0 +1,11 @@
+--------------------------------------------------------------
+-- FileName:  learning_7_3.lua
+-- author: 此文件為自動生成，沒有作者。有問題請 @郭浩
+--------------------------------------------------------------
+ui_learning_7_3 = {};
+local p =  ui_learning_7_3;
+p.ID_CTRL_BUTTON_CALLBACK_1			= 5;
+p.ID_CTRL_PICTURE_27					     = 27;
+p.ID_CTRL_PICTURE_3					      = 3;
+p.ID_CTRL_PICTURE_2					      = 2;
+p.ID_CTRL_PICTURE_1					      = 1;
