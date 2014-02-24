af5fcaab9da19b7227470e70ee297a515d0b1995
csd <csd@test.com>
2014-02-24 10:06:12 +0800

    引导触发

--- gameres/res/script/ui/ui_learning_5_20.lua
+++ gameres/res/script/ui/ui_learning_5_20.lua
@@ -4,9 +4,9 @@
 --------------------------------------------------------------
 ui_learning_5_20 = {};
 local p =  ui_learning_5_20;
+p.ID_CTRL_BUTTON_CALLBACK_1			= 3;
 p.ID_CTRL_BUTTON_14					      = 14;
 p.ID_CTRL_PICTURE_12					     = 12;
 p.ID_CTRL_PICTURE_11					     = 11;
 p.ID_CTRL_PICTURE_10					     = 10;
-p.ID_CTRL_BUTTON_CALLBACK_1			= 3;
 p.ID_CTRL_PICTURE_1					      = 1;
