daa69da2840349b096c4946a8978bcd9c71fca5a
csd <csd@test.com>
2014-02-24 11:50:04 +0800

    引导中的HP和SP水晶

--- gameres/res/script/ui/ui_learning_3_9.lua
+++ gameres/res/script/ui/ui_learning_3_9.lua
@@ -5,6 +5,7 @@
 ui_learning_3_9 = {};
 local p =  ui_learning_3_9;
 p.ID_CTRL_BUTTON_CALLBACK_1			= 3;
+p.ID_CTRL_PICTURE_9					      = 9;
 p.ID_CTRL_TEXT_66						       = 66;
 p.ID_CTRL_PICTURE_65					     = 65;
 p.ID_CTRL_PICTURE_1					      = 1;
