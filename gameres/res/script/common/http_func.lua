ca094f574956670fbe8b41f7c66e2c4b13c62d0a
crj <111111>
2014-02-25 15:09:47 +0800

    请求服务端数据，20s未返回，重新请求

--- gameres/res/script/common/http_func.lua
+++ gameres/res/script/common/http_func.lua
@@ -1,7 +1,7 @@
 --------------------------------------------------------------
 -- FileName: 	http_func.lua
 -- author:		
--- purpose:		http��������
+-- purpose:		http辅助函数
 --------------------------------------------------------------
 
 local http_busy = false;
@@ -11,10 +11,12 @@ local WaitForPostCallBack = nil;
 local RequestCallBack = nil;
 local pLayer = nil;
 
---����Http����
+local resendTimer = nil;
+
+--发送Http请求
 function SendReq( cmd, action, uid, param )
 	if WaitForPostCallBack ~= nil then
-		local flag = WaitForPostCallBack();--��Ҫ����ֵ���Ƿ��ѷ���post����
+		local flag = WaitForPostCallBack();--需要返回值，是否已发送post请求
 		if flag then
 			WaitForPostCallBack = nil;
 			RequestCallBack = { Cmd = cmd, Action = action, Uid = uid, Param = param };
@@ -42,6 +44,13 @@ function SendReq( cmd, action, uid, param )
 	http_busy = true;
 	
 	rookie_mask.onCallFlag = true;
+	
+	--注册重新发送请求定时器
+	if resendTimer ~= nil then
+		KillTimer( resendTimer );
+		resendTimer = nil;
+	end
+	resendTimer = SetTimerOnce( OnReSendReq, 20.0f );
 end
 
 function SendPost(cmd, action, uid, param,data)
@@ -51,7 +60,7 @@ function SendPost(cmd, action, uid, param,data)
 	http_busy = true;
 end
 
---����Ƿ�æ
+--检查是否忙
 function OnTimerCheckBusy()
 	if http_busy then
 		--if not FindHudEffect(busy_fx) then
@@ -68,6 +77,13 @@ function HttpOK()
 	http_busy = false;
 	rookie_mask.onCallFlag = false;
 	--DelHudEffect( busy_fx );
+	
+	--注销重新发送请求定时器
+	if resendTimer ~= nil then
+		KillTimer( resendTimer );
+		resendTimer = nil;
+	end
+	
 	if pLayer ~= nil then
 		pLayer:SetSwallowTouch( false );
 		if pLayer:HasAniEffect( busy_fx ) then
@@ -76,16 +92,32 @@ function HttpOK()
 	end
 end
 
---���õȴ�post�Ľӿ�
+--设置等待post的接口
 function SetWaitForPost( pCall )
 	WaitForPostCallBack = pCall;
 end
 
---post�ɹ��ص�
+--post成功回调
 function PostBack()
 	--WaitForPostCallBack = nil;
 	if RequestCallBack ~= nil and RequestCallBack.Cmd ~= nil and RequestCallBack.Action ~= nil and RequestCallBack.Uid ~= nil and RequestCallBack.Param ~= nil then
 		SendReq(RequestCallBack.Cmd, RequestCallBack.Action, RequestCallBack.Uid, RequestCallBack.Param );
 	end
+end	
+
+function OnReSendReq()
+	resendTimer = nil;
+	--去除loading图
+	if pLayer ~= nil then
+		pLayer:SetSwallowTouch( false );
+		if pLayer:HasAniEffect( busy_fx ) then
+			pLayer:DelAniEffect( busy_fx );
+		end
+	end
+
+	dlg_msgbox.ShowOK( "错误", "长时间未操作，与服务器断开连接，请重新连接。", OnClickReSend, GetUIRoot() );
 end
 
+function OnClickReSend()
+	ResendRequest();
+end
