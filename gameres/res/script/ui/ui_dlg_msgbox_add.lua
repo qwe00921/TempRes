42bc78016fe187ae6acd772b239333ccac06e116
sgkqk <kqk@qq.com>
2014-02-28 11:37:17 +0800

    xui调整

---
+++ gameres/res/script/ui/ui_dlg_msgbox_add.lua
@@ -0,0 +1,15 @@
+--------------------------------------------------------------
+-- FileName:  dlg_msgbox_add.lua
+-- author: 此文件為自動生成，沒有作者。有問題請 @郭浩
+--------------------------------------------------------------
+ui_dlg_msgbox_add = {};
+local p =  ui_dlg_msgbox_add;
+p.ID_CTRL_TEXT_20						       = 20;
+p.ID_CTRL_PICTURE_19					     = 19;
+p.ID_CTRL_BUTTON_NO					      = 6;
+p.ID_CTRL_BUTTON_YES					     = 5;
+p.ID_CTRL_TEXT_3						        = 3;
+p.ID_CTRL_9SLICES_10					     = 11;
+p.ID_CTRL_9SLICES_49					     = 49;
+p.ID_CTRL_9SLICES_8					      = 8;
+p.ID_CTRL_9SLICES_9					      = 9;
